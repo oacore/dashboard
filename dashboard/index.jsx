ab07adb5bd7cf898d70c5c4349731e085ea37a85
Jozef Harag <32jojo32@gmail.com>
2019-11-17 13:41:40 +0100

    Fix eslint

--- dashboard/index.jsx
+++ dashboard/index.jsx
@@ -1,5 +1,3 @@
-import React  from 'react'
-
 const CoreDashboard = () => {
   return null
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,31 @@
 import React from 'react'
 import Head from 'next/head'
 
+const Dashboard = () => {
+  return (
+    <>
+      <Head>
+        <title>CORE Dashboard</title>
 
-class Dashboard extends React.Component {
-  render() {
-    return (
-      <>
-        <Head>
-          <title>CORE Dashboard</title>
-
-          {['64', '128', '256', '512'].map(size => (
-            <link
-              key={size}
-              rel="icon"
-              type="image/png"
-              sizes={`${size}x${size}`}
-              href={`/favicon/favicon-${size}px.png`}
-            />
-          ))}
-
+        {['64', '128', '256', '512'].map(size => (
           <link
+            key={size}
             rel="icon"
-            sizes="any"
-            type="image/svg+xml"
-            href='/favicon/favicon.svg'
+            type="image/png"
+            sizes={`${size}x${size}`}
+            href={`/favicon/favicon-${size}px.png`}
           />
-        </Head>
-      </>
-    )
-  }
+        ))}
+
+        <link
+          rel="icon"
+          sizes="any"
+          type="image/svg+xml"
+          href="/favicon/favicon.svg"
+        />
+      </Head>
+    </>
+  )
 }
 
 export default Dashboard
