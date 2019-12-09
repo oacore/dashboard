da224101c76cd50d703150a99c8cd0a3eac438c3
Joozty <32jojo32@gmail.com>
2019-12-09 09:58:10 +0100

    Use postcss-custom-media

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,20 @@
+const path = require('path')
+
 const presetEnv = require('postcss-preset-env')
 const extend = require('postcss-extend')
 const nano = require('cssnano')
+const postcssCustomMedia = require('postcss-custom-media')
 
 module.exports = {
   map: true,
   plugins: [
     presetEnv(),
+    postcssCustomMedia({
+      importFrom: path.resolve(
+        __dirname,
+        'components/layout/media-queries.css'
+      ),
+    }),
     extend(),
     nano({
       preset: [
