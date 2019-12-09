cc06b6115c9295f9da0a904d83aad1a4d8cc16f8
Joozty <32jojo32@gmail.com>
2019-12-09 12:09:25 +0100

    Remove Analytics & Reader plugins

--- pages/plugins.jsx
+++ pages/plugins.jsx
@@ -5,22 +5,9 @@ import pluginsClassNames from './plugins.css'
 
 const integrations = [
   {
-    name: 'Analytics',
-    abstract:
-      'Enhance your statistics by enabling collecting data from your website',
-    description:
-      'Increase your website discoverability by cross-linking similar papers with help of our powerful recommender system',
-  },
-  {
     name: 'Discovery',
     abstract:
       'Enrich your full text amount by dynamic adding missing bits from CORE',
-    description:
-      'Increase your website discoverability by cross-linking similar papers with help of our powerful recommender system',
-  },
-  {
-    name: 'Reader',
-    abstract: 'Enable future reading experience for the files on your website',
     description:
       'Increase your website discoverability by cross-linking similar papers with help of our powerful recommender system',
   },
