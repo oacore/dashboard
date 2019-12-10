ebb506f3450f8f9ca69e357ec62e034498a1ada9
Joozty <32jojo32@gmail.com>
2019-12-10 17:16:49 +0100

    Display some demo data

--- pages/data.jsx
+++ pages/data.jsx
@@ -1,16 +1,55 @@
 import React from 'react'
+import { TextField } from '@oacore/design'
+
+import Table from '../components/table'
+import { range } from '../utils/helpers'
+import dataClassNames from './data.css'
+
+const sleep = () => {
+  return new Promise(resolve => {
+    // wait 3s before calling fn(par)
+    setTimeout(() => resolve(), 3000)
+  })
+}
 
 const Data = () => {
+  const columns = [
+    'select',
+    'oai',
+    'title',
+    'authors',
+    'last update',
+    'Visibility',
+  ]
+
+  const fetchData = async () => {
+    // We'll even set a delay to simulate a server here
+    await sleep()
+    return range(100).map(() => ({
+      oai: Math.random(),
+      title:
+        'Zero and low carbon buildings: A drive for change' +
+        ' in working practices and the use of computer modelling',
+      authors: 'Robina Hetherington, Robin Laney, Stephen Peake',
+      lastUpdate: '12 days ago',
+      visibility: 'visibility',
+    }))
+  }
+
   return (
-    <>
-      <h1>Dashboard data tab</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia
-        numquam aliquid, blanditiis praesentium doloribus molestiae sequi facere
-        quasi minima odio officia maxime accusantium iusto ducimus labore ut
-        nemo perferendis libero.
-      </p>
-    </>
+    <div className={dataClassNames.dataTab}>
+      <TextField
+        id="search"
+        type="search"
+        name="search"
+        label="Search"
+        placeholder="Any identifier, title, author..."
+      />
+      <Table
+        columns={columns}
+        fetchData={fetchData}
+      />
+    </div>
   )
 }
 
