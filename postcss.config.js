e29a8a92d53039fb918a21dd4364fddfa5ea38a1
Viktor Yakubiv <viktor@yakubiv.com>
2019-12-16 18:33:34 +0200

    Setup postcss-preset-env
    Sets stage to 0.
    
    Removes explicit usage of custom-media.

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,12 @@
-const path = require('path')
-
 const presetEnv = require('postcss-preset-env')
 const extend = require('postcss-extend')
 const nano = require('cssnano')
-const postcssCustomMedia = require('postcss-custom-media')
 
 module.exports = {
   map: true,
   plugins: [
-    presetEnv(),
-    postcssCustomMedia({
-      importFrom: path.resolve(
-        __dirname,
-        'components/layout/media-queries.css'
-      ),
+    presetEnv({
+      stage: 0,
     }),
     extend(),
     nano({
