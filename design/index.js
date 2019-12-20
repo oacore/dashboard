53c4703300720f18ae53c1cb0c061135f4cc5b33
Viktor Yakubiv <viktor@yakubiv.com>
2019-12-20 16:42:02 +0200

    Add helper classes
    API is not really good but may be improved later.

--- design/index.js
+++ design/index.js
@@ -3,3 +3,5 @@ export Avatar from './avatar'
 export CloseIcon from './close-icon'
 export Drawer from './drawer'
 export Logo from './logo'
+
+export classNameHelpers from './class-name-helpers.css'
