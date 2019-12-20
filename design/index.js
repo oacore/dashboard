6d7f9b4dae67eaa85659a17086c5ce19f70a43fa
Viktor Yakubiv <viktor@yakubiv.com>
2019-12-20 13:58:05 +0200

    Add basic Card component

--- design/index.js
+++ design/index.js
@@ -1,5 +1,6 @@
 export AppBarToggle from './app-bar/toggle'
 export Avatar from './avatar'
+export Card from './card'
 export CloseIcon from './close-icon'
 export Drawer from './drawer'
 export Logo from './logo'
