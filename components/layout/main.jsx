fe558ab78df56c55f8994eb02ce33aeb1f8a6ed2
Viktor Yakubiv <viktor@yakubiv.com>
2020-01-02 18:09:35 +0200

    Make Layout.Main a HOC that wraps a child
    Layout.Main passes down className and tag props.
    
    Fixes a scrollbar appearing under app-bar.
    
    Fixes scroll flow, improves scroll experience.

--- components/layout/main.jsx
+++ components/layout/main.jsx
@@ -3,10 +3,13 @@ import { classNames } from '@oacore/design/lib/utils'
 
 import styles from './styles.css'
 
-const Main = ({ children, className, tag: Tag = 'main', ...restProps }) => (
-  <Tag className={classNames.use(styles.main).join(className)} {...restProps}>
-    {children}
-  </Tag>
-)
+const Main = ({ children }) => {
+  const child = React.Children.only(children)
+  const { className, tag } = child.props
+  return React.cloneElement(child, {
+    className: classNames.use(styles.main).join(className),
+    tag: tag || 'main',
+  })
+}
 
 export default Main
