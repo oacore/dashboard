35dee2e8aebec9da2a7e6bca6f23482448299344
Joozty <32jojo32@gmail.com>
2020-01-09 16:18:23 +0100

    Connect DepositDates table to API

--- pages/data-providers/[data-provider-id]/deposit-dates.jsx
+++ pages/data-providers/[data-provider-id]/deposit-dates.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import { Label, Button } from '@oacore/design'
 
+import { withGlobalStore } from '../../../store'
+
 import Table from 'components/table'
-import { range } from 'utils/helpers'
 import { Card } from 'design'
 
 // TODO: Remove once cards are in @oacore/design
 // eslint-disable-next-line
 import styles from './deposit-dates.css'
-
-const sleep = () =>
-  new Promise(resolve => {
-    // wait 3s before calling fn(par)
-    setTimeout(() => resolve(), 3000)
-  })
 
 const tableConfig = {
   columns: [
@@ -23,17 +18,21 @@ const tableConfig = {
       sortable: true,
       expandedSize: 2,
       className: styles.labelColumn,
-      render: v => <Label color="primary">{v.short}</Label>,
+      render: (v, isExpanded) => (
+        <Label color="primary">{isExpanded ? v : v.split(':').pop()}</Label>
+      ),
     },
     {
       id: 'title',
       display: 'Title',
       order: 'asc',
+      className: styles.titleColumn,
     },
     {
-      id: 'authors',
+      id: 'author',
       display: 'Authors',
       order: '',
+      className: styles.authorsColumn,
     },
     {
       id: 'deposit-at',
@@ -43,67 +42,75 @@ const tableConfig = {
   ],
 }
 
-const fetchData = async () => {
-  // We'll even set a delay to simulate a server here
-  await sleep()
-  return range(100).map(() => ({
-    id: Math.random(),
-    oai: {
-      short: 'oai',
-      long: Math.random(),
-    },
-    title:
-      'Zero and low carbon buildings: A drive for change' +
-      ' in working practices and the use of computer modelling',
-    authors: 'Robina Hetherington, Robin Laney, Stephen Peake',
-    'deposit-at': '12 days ago',
-  }))
-}
-
 /**
  * TODO: This is an example of bad design. We have to know structure
  *       of Layout.Main and explicitly pass props.
  *       We should get rid out of it!
  */
-const DepositDates = ({ className, tag: Tag = 'main', ...restProps }) => (
-  <Tag className={[styles.container, className].join(' ')} {...restProps}>
-    <h1>Deposit compliance</h1>
+const DepositDates = ({
+  className,
+  store,
+  tag: Tag = 'main',
+  ...restProps
+}) => {
+  const fetchData = async (pageNumber, searchTerm, columnOrder) => {
+    const page = await store.works.retrieveWorks(
+      pageNumber,
+      searchTerm,
+      columnOrder
+    )
 
-    <Card className={styles.complianceLevel} tag="section">
-      <h2>Compliance level</h2>
-      <p>
-        Your deposit compliance is <strong>90%</strong>.
-      </p>
-      <p>
-        Deposit compliance level is a percentage of works that has been
-        deposited at least in first 90 days after publishing.
-      </p>
-    </Card>
+    return page.data.map(v => ({
+      id: v.id,
+      oai: v.identifier.oai,
+      title: v.title,
+      author: v.author.map(a => a.name).join(' '),
+      'deposit-at': '12 days ago',
+    }))
+  }
 
-    <Card className={styles.export} tag="section">
-      <h2>Export</h2>
-      <p>
-        We have deposit dates for <b>182, 719</b> out of 183,219 works within
-        your repository
-      </p>
-      <Button variant="contained">Download</Button>
-    </Card>
+  return (
+    <Tag className={[styles.container, className].join(' ')} {...restProps}>
+      <h1>Deposit compliance</h1>
 
-    <Card className={styles.depositTimeLag} tag="section">
-      <h2>Deposit Time Lag</h2>
-      <p>The chart diplays work distribution per deposit time lag in days.</p>
-    </Card>
+      <Card className={styles.complianceLevel} tag="section">
+        <h2>Compliance level</h2>
+        <p>
+          Your deposit compliance is <strong>90%</strong>.
+        </p>
+        <p>
+          Deposit compliance level is a percentage of works that has been
+          deposited at least in first 90 days after publishing.
+        </p>
+      </Card>
 
-    <Card className={styles.browseTable} tag="section">
-      <Table
-        className={styles.table}
-        title="Browse deposit dates"
-        selectable
-        config={tableConfig}
-        fetchData={fetchData}
-      />
-    </Card>
-  </Tag>
-)
+      <Card className={styles.export} tag="section">
+        <h2>Export</h2>
+        <p>
+          We have deposit dates for <b>182, 719</b> out of 183,219 works within
+          your repository
+        </p>
+        <Button variant="contained">Download</Button>
+      </Card>
 
-export default DepositDates
+      <Card className={styles.depositTimeLag} tag="section">
+        <h2>Deposit Time Lag</h2>
+        <p>
+          The chart displays work distribution per deposit time lag in days.
+        </p>
+      </Card>
+
+      <Card className={styles.browseTable} tag="section">
+        <Table
+          className={styles.table}
+          title="Browse deposit dates"
+          selectable
+          config={tableConfig}
+          fetchData={fetchData}
+        />
+      </Card>
+    </Tag>
+  )
+}
+
+export default withGlobalStore(DepositDates)
