1e7f46fd64eaa8d4e56c9e83bd9a61e91237bbf9
Joozty <32jojo32@gmail.com>
2020-01-21 16:27:59 +0100

    Patch deposit dates chart with missing zeros

--- components/time-lag-chart/index.jsx
+++ components/time-lag-chart/index.jsx
@@ -40,33 +40,53 @@ const formatter = value => {
 }
 
 const ticks = [-365, -31, -7, 0, 7, 31, 90, 365]
-const TimeLagChart = React.memo(({ data }) => (
-  <ResponsiveContainer width="100%" height={300}>
-    <BarChart margin={{ bottom: -5 }} data={data}>
-      <XAxis
-        dataKey="depositTimeLag"
-        tickLine={false}
-        ticks={ticks}
-        tickFormatter={formatter}
-      />
-      <Tooltip />
-      <ReferenceLine y={0} className={styles.referenceLine} />
-      <Bar dataKey="worksCount">
-        {data.map(entry => (
-          <Cell
-            className={classNames
-              .use({
-                'lag-bar': true,
-                compliant: entry.depositTimeLag <= 90,
-              })
-              .from(styles)
-              .toString()}
-            key={entry.depositTimeLag}
-          />
-        ))}
-      </Bar>
-    </BarChart>
-  </ResponsiveContainer>
-))
+const TimeLagChart = React.memo(({ data }) => {
+  const normalizedData = []
+  for (
+    let i = 0;
+    i < data[data.length - 1].depositTimeLag - data[0].depositTimeLag;
+    i++
+  ) {
+    const lagIndex = i + data[0].depositTimeLag
+    const el = data.find(e => e.depositTimeLag === lagIndex)
+    if (el) normalizedData.push(el)
+    else {
+      normalizedData.push({
+        depositTimeLag: lagIndex,
+        worksCount: 0,
+      })
+    }
+  }
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart margin={{ bottom: -5 }} data={normalizedData}>
+        <XAxis
+          type="numeric"
+          dataKey="depositTimeLag"
+          tickLine={false}
+          ticks={ticks}
+          tickFormatter={formatter}
+        />
+        <Tooltip />
+        <ReferenceLine y={0} className={styles.referenceLine} />
+        <Bar dataKey="worksCount">
+          {normalizedData.map(entry => (
+            <Cell
+              className={classNames
+                .use({
+                  'lag-bar': true,
+                  compliant: entry.depositTimeLag <= 90,
+                })
+                .from(styles)
+                .toString()}
+              key={entry.depositTimeLag}
+            />
+          ))}
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
+  )
+})
 
 export default TimeLagChart
