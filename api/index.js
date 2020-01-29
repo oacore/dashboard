1f782788c864954ac81ed3973261d4eb384a7177
Joozty <32jojo32@gmail.com>
2020-01-29 12:08:39 +0100

    Rewrite apiRequest to pure promise

--- api/index.js
+++ api/index.js
@@ -10,57 +10,69 @@ const apiRequest = async (
   customHeaders = {},
   dev = false
 ) => {
-  try {
-    const requestUrl = new URL(
-      /^\w+:\/\//.test(url) ? url : `${dev ? CORE_API_DEV : CORE_API}${url}`
-    )
-    const requestHeaders = {
-      Accept: 'application/json',
-      ...customHeaders,
-    }
+  const requestUrl = new URL(
+    /^\w+:\/\//.test(url) ? url : `${dev ? CORE_API_DEV : CORE_API}${url}`
+  )
+  const requestHeaders = {
+    Accept: 'application/json',
+    ...customHeaders,
+  }
 
-    requestUrl.search = new URLSearchParams(queryParams).toString()
+  requestUrl.search = new URLSearchParams(queryParams).toString()
 
-    const response = await fetch(requestUrl, {
-      method,
-      credentials: 'include',
-      headers: requestHeaders,
+  const fetchPromise = fetch(requestUrl, {
+    method,
+    credentials: 'include',
+    headers: requestHeaders,
+  })
+    .then(response => {
+      const { headers } = response
+      const type = headers.get('Content-Type')
+      const result = { type, headers }
+
+      if (response.status >= 400)
+        throw new NetworkError(`Request failed on ${response.status}`, response)
+
+      if (method.toUpperCase() === 'HEAD') return result
+
+      const dataPromise = /application\/([\w.-]\+)?json/g.test(type)
+        ? response.json()
+        : response.blob()
+
+      return new Promise((resolve, reject) =>
+        dataPromise.then(
+          value =>
+            resolve({
+              data: value,
+              type,
+              headers,
+            }),
+          reason => reject(reason)
+        )
+      )
+    })
+    .catch(async error => {
+      const { response, message } = error
+      const { text } = await response.text()
+      let networkError
+      if (response) {
+        networkError = new NetworkError(
+          `Request for ${method} ${url} failed. Response: ${response.status}, ${text}`
+        )
+      } else if (message === 'Network Error') {
+        networkError = new NetworkError(
+          `Request ${method} ${url} failed. You are probably in offline mode.`
+        )
+      } else {
+        networkError = new NetworkError(
+          `Request ${method} ${url} failed. The original error was: ${message}`
+        )
+      }
+      networkError.status = (response && response.status) || null
+      throw networkError
     })
 
-    const { headers } = response
-    const type = headers.get('Content-Type')
-    const result = { type, headers }
-
-    if (response.status >= 400)
-      throw new NetworkError(`Request failed on ${response.status}`, response)
-
-    if (method.toUpperCase() === 'HEAD') return result
-
-    result.data = await (/application\/([\w.-]\+)?json/g.test(type)
-      ? response.json()
-      : response.blob())
-
-    return result
-  } catch (error) {
-    const { response, message } = error
-    const { text } = await response.text()
-    let networkError
-    if (response) {
-      networkError = new NetworkError(
-        `Request for ${method} ${url} failed. Response: ${response.status}, ${text}`
-      )
-    } else if (message === 'Network Error') {
-      networkError = new NetworkError(
-        `Request ${method} ${url} failed. You are probably in offline mode.`
-      )
-    } else {
-      networkError = new NetworkError(
-        `Request ${method} ${url} failed. The original error was: ${message}`
-      )
-    }
-    networkError.status = (response && response.status) || null
-    throw networkError
-  }
+  return fetchPromise
 }
 
 export default apiRequest
