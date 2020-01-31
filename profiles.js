6009543884eb301d79ed4fe4e9c7c8ef43490077
Joozty <32jojo32@gmail.com>
2020-01-31 12:43:24 +0100

    Introduce profiles

---
+++ profiles.js
@@ -0,0 +1,13 @@
+const profiles = {
+  localhost: {
+    apiURL: 'https://localhost:8000',
+  },
+  staging: {
+    apiURL: 'https://api.dev.core.ac.uk',
+  },
+  production: {
+    apiURL: 'https://api.core.ac.uk',
+  },
+}
+
+export default profiles
