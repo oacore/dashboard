8c66a174a079f6b4bb7a00a2b0a27d666d897684
Viktor Yakubiv <viktor@yakubiv.com>
2020-02-10 14:06:02 +0200

    Create a development area

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
-export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://api.core.ac.uk/internal'
-    : 'https://api.dev.core.ac.uk/internal'
+export const API_URL = 'https://api.dev.core.ac.uk/internal'
 
 const ORIGIN = encodeURIComponent(
   typeof window == 'object' ? window.location.origin : ''
 )
 
-export const LOGIN_URL =
-  process.env.NODE_ENV === 'production'
-    ? '/login.html'
-    : new URL(`/test/login?continue=${ORIGIN}`, API_URL).toString()
+export const LOGIN_URL = new URL(
+  `/test/login?continue=${ORIGIN}`,
+  API_URL
+).toString()
