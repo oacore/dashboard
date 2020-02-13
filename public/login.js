9a45db860e9a299f4bd90acc7a18737f347e86b0
Joozty <32jojo32@gmail.com>
2020-02-13 09:46:36 +0100

    Apply CSP on login page
    1. Inline code extracted to separate files
    See: https://developers.google.com/web/fundamentals/security/csp
    
    2. Only scripts from self and *.core.ac.uk are allowed

---
+++ public/login.js
@@ -0,0 +1,14 @@
+function login(e) {
+  e.target.action = `https://api.core.ac.uk/login_check?continue=${encodeURIComponent(
+    window.location.origin
+  )}`
+}
+
+const urlParams = new URLSearchParams(window.location.search)
+if (urlParams.has('reason')) {
+  const message = document.getElementById('error-message')
+  message.innerHTML = 'The username or password you entered is incorrect.'
+}
+
+const form = document.getElementById('login-form')
+form.addEventListener('submit', login)
