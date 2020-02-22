c0ece1d89d16fc07cd9eab86e15fd5133d944b1f
Joozty <32jojo32@gmail.com>
2020-02-22 17:09:29 +0100

    Load data in store for content table

--- store/works.js
+++ store/works.js
@@ -1,44 +1,9 @@
-import Page from './helpers/page'
-import getOrder from './helpers/order'
-import { CancelablePromise } from '../utils/promise'
-import invalidatePreviousRequests from './helpers/invalidatePreviousRequests'
+import { Pages } from './helpers/pages'
 
-import apiRequest from 'api'
-
-const PAGE_SIZE = 100
-
-class Works {
+class Works extends Pages {
   constructor(baseUrl) {
-    this.worksUrl = `${baseUrl}/works`
-  }
-
-  @invalidatePreviousRequests
-  retrieveWorks(pageNumber, searchTerm, columnOrder) {
-    const order = getOrder(columnOrder)
-
-    const params = {
-      from: pageNumber * PAGE_SIZE,
-      size: PAGE_SIZE,
-    }
-    if (order) params.orderBy = order
-    if (searchTerm) params.q = searchTerm
-    const request = apiRequest(this.worksUrl, 'GET', params, {}, true)
-    const dataPromise = new Promise((resolve, reject) =>
-      request.promise.then(
-        ({ data }) => {
-          const page = new Page(data, {
-            searchTerm,
-            order,
-            maxSize: PAGE_SIZE,
-          })
-          resolve(page)
-        },
-        reason => reject(reason)
-      )
-    )
-    return new CancelablePromise(dataPromise, {
-      cancel: request.cancel,
-    })
+    const url = `${baseUrl}/works`
+    super(url)
   }
 }
 
