2a9844324f4a3a306beed85d10ba7bbbcb0828a5
Joozty <32jojo32@gmail.com>
2020-02-22 17:02:50 +0100

    Create LinkedList helper

---
+++ store/helpers/linkedList/linkedList.js
@@ -0,0 +1,58 @@
+import LinkedListNode from './linkedListNode'
+
+export default class LinkedList {
+  constructor() {
+    this.head = null
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  next() {
+    throw new Error('next method has to be implemented')
+  }
+
+  add(data) {
+    const node = new LinkedListNode(data)
+
+    // no data loaded yet
+    if (this.head === null) this.head = node
+    else {
+      // find the last node and insert data there
+      let current = this.head
+      while (current.next !== null) current = current.next
+      current.next = node
+    }
+  }
+
+  get(index) {
+    let current = this.head
+    let i = 0
+
+    while (current !== null && i < index) {
+      current = current.next
+      i += 1
+    }
+
+    // return the data if `current` isn't null
+    return current !== null ? current.data : undefined
+  }
+
+  get length() {
+    let current = this.head
+    let length = 0
+    while (current !== null) {
+      length += current.data.data.length
+      current = current.next
+    }
+    return length
+  }
+
+  get data() {
+    let current = this.head
+    const data = []
+    while (current !== null) {
+      data.push(...current.data.data)
+      current = current.next
+    }
+    return data
+  }
+}
