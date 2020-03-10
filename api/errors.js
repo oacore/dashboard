c52566d56053c25570eb20b7a5b15200d80965e8
Joozty <32jojo32@gmail.com>
2020-03-10 11:54:53 +0100

    Create custom network errors

--- api/errors.js
+++ api/errors.js
@@ -1,8 +1,27 @@
-/* eslint-disable import/prefer-default-export */
-
+// eslint-disable-next-line max-classes-per-file
 export class NetworkError extends Error {
   constructor(message, response) {
     super(message)
+    this.name = this.constructor.name
     this.response = response
   }
 }
+
+export class UnauthorizedError extends NetworkError {}
+export class PaymentRequiredError extends NetworkError {}
+export class ForbiddenError extends NetworkError {}
+export class NotFoundError extends NetworkError {}
+export class BadRequestError extends NetworkError {}
+export class InternalServerError extends NetworkError {}
+
+NetworkError.getErrorFromStatusCode = statusCode => {
+  if (statusCode === 401) return UnauthorizedError
+  if (statusCode === 401) return PaymentRequiredError
+  if (statusCode === 403) return ForbiddenError
+  if (statusCode === 404) return NotFoundError
+  if (statusCode >= 400 && statusCode < 500) return BadRequestError
+  if (statusCode >= 500) return InternalServerError
+  return NetworkError
+}
+
+export default NetworkError
