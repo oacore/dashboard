0e5f6b04db1e5600a632d16d67d457a4bab22d8a
Jozef Harag <32jojo32@gmail.com>
2020-03-12 13:33:04 +0100

    Fix login redirect (#183)

--- config.js
+++ config.js
@@ -3,8 +3,16 @@ export const API_URL =
     ? 'https://api.core.ac.uk/internal'
     : 'https://api.dev.core.ac.uk/internal'
 
-export const getLoginPage = (path = window.location.origin) =>
-  new URL(
+export const getLoginPage = (path = window.location.origin) => {
+  if (process.env.NODE_ENV === 'production') {
+    return new URL(
+      `/login.html?continue=${encodeURIComponent(path)}`,
+      window.location.origin
+    ).toString()
+  }
+
+  return new URL(
     `/test/login?continue=${encodeURIComponent(path)}`,
-    process.env.NODE_ENV === 'production' ? undefined : API_URL
+    API_URL
   ).toString()
+}
