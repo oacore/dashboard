74207d489cee1792eb96034c00cadd990d28bad9
Viktor Yakubiv <viktor@yakubiv.com>
2020-03-17 23:51:12 +0200

    Add generic Resource
    A Resource instance can fetch itself from the API via
    URL and resolve nested scopes, e.g. can fetch `dataProviders`
    if there is a `dataProvidersUrl`.

---
+++ store/resource.js
@@ -0,0 +1,73 @@
+import { action, observable } from 'mobx'
+
+import apiRequest from 'api'
+
+class Resource {
+  static defaultOptions = {
+    request: (...args) => apiRequest(args).then(({ data }) => data),
+    prefetch: false,
+  }
+
+  static defaultValues = {}
+
+  constructor(init, options) {
+    this.options = { ...this.constructor.defaultOptions, ...options }
+
+    this.extend(this.constructor.defaultValues)
+    this.init(init)
+  }
+
+  @action
+  init(init) {
+    const data = typeof init == 'string' ? { url: init } : init
+    this.extend(data)
+
+    const { prefetch } = this.options
+    if (typeof init == 'string' && prefetch) this.retrieve()
+  }
+
+  @action
+  extend(object) {
+    Object.entries(object).forEach(([key, value]) => {
+      this[key] = key in this ? value : observable(value)
+    })
+  }
+
+  retrieve(...scopes) {
+    const { request } = this.options
+    if (this.id == null) {
+      return request(this.url).then((data) => {
+        this.extend(data)
+        return this.retrieve(...scopes)
+      })
+    }
+
+    const requests = scopes.map((scope) => {
+      const scopeUrl = `${scope}Url`
+
+      if (this[scopeUrl] == null) {
+        throw new Error(
+          `Trying to retrieve non-existing nested property '${scope}' via '${scopeUrl}'`
+        )
+      }
+
+      return request(this[scopeUrl]).then((data) => {
+        this.extend({ [scope]: data })
+        return data
+      })
+    })
+
+    return Promise.allSettled(requests)
+  }
+
+  valueOf() {
+    return this.id
+  }
+
+  toString() {
+    const name = this.constructor.name || 'Resource'
+    return `${name}#${this.id}`
+  }
+}
+
+export default Resource
