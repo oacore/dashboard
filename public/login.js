ed2861a3fdb5652f14823afc73c573af3066a8c9
Jozef Harag <32jojo32@gmail.com>
2020-03-19 09:48:06 +0100

    Use identity_provider_url if provided
    & show wrong credentials when 401 occurs

--- public/login.js
+++ public/login.js
@@ -1,22 +1,3 @@
-function login(event) {
-  event.preventDefault()
-  window.dispatchEvent(new Event('login-processing'))
-  const formData = new FormData(event.target)
-  const data = new URLSearchParams(formData)
-  fetch('https://api.core.ac.uk/login_check', {
-    method: 'POST',
-    body: data,
-    credentials: 'include',
-  }).finally(() => {
-    if (window.location !== window.parent.location) {
-      // The page is in an iframe
-      window.parent.postMessage('login-processing', window.location.origin)
-    } else window.location = '/'
-  })
-
-  return false
-}
-
 function addLoadingAnimation() {
   const form = document.getElementById('login-form')
   form.classList.add('loading')
@@ -31,27 +12,63 @@ function handlePostMessage(event) {
   if (event.data === 'login-finished') removeLoadingAnimation()
 }
 
+function showWrongCredentialsMessage() {
+  const message = document.getElementById('message')
+  message.innerHTML = 'The username or password you entered is incorrect.'
+  message.classList.add('message-error')
+  message.classList.remove('message-info')
+}
+
+function showLogoutMessage() {
+  const message = document.getElementById('message')
+  message.innerHTML = 'You have been successfully logged out. Log in again.'
+  message.classList.add('message-info')
+  message.classList.remove('message-error')
+}
+
+function login(event) {
+  event.preventDefault()
+  const urlParams = new URLSearchParams(window.location.search)
+  const identityProviderUrl =
+    urlParams.get('identity_provider_url') || 'https://api.core.ac.uk/'
+
+  window.dispatchEvent(new Event('login-processing'))
+  const formData = new FormData(event.target)
+  const data = new URLSearchParams(formData)
+  fetch(`${identityProviderUrl}login_check`, {
+    method: 'POST',
+    body: data,
+    credentials: 'include',
+  })
+    .then(r => {
+      if (r.status === 401) showWrongCredentialsMessage()
+    })
+    .finally(() => {
+      if (window.location !== window.parent.location) {
+        // The page is in an iframe
+        window.parent.postMessage('login-processing', window.location.origin)
+      } else window.location = '/'
+    })
+
+  return false
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   const urlParams = new URLSearchParams(window.location.search)
   const reason = urlParams.get('reason')
   if (reason) {
-    const message = document.getElementById('message')
-
     switch (reason) {
       case 'wrong_credentials':
-        message.innerHTML = 'The username or password you entered is incorrect.'
-        message.classList.add('message-error')
+        showWrongCredentialsMessage()
         break
       case 'logout':
-        message.innerHTML =
-          'You have been successfully logged out. Log in again.'
-        message.classList.add('message-info')
+        showLogoutMessage()
         break
       default:
     }
   }
 
-  if (urlParams.has('loading')) addLoadingAnimation()
+  if (urlParams.get('loading') === 'true') addLoadingAnimation()
 
   const form = document.getElementById('login-form')
   form.addEventListener('submit', login)
