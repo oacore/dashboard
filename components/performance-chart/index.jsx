ef036bfe1bf69ffa718221872cb69467639c29d7
Jozef Harag <32jojo32@gmail.com>
2020-03-24 14:48:43 +0100

    Create PerformanceChart component
    Extracts RadialChart component from Overview page.
    
    Adds a second bar to the PerformanceChart.
    
    Fixes working with non-numeric values.
    
    Fixes chart spacing. Adds another wrapper and fixed ratio container.
    There is no other way to do it at the moment.
    
    Co-authored-by: Viktor Yakubiv <viktor@yakubiv.com>

---
+++ components/performance-chart/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { RadialBar, RadialBarChart, ResponsiveContainer } from 'recharts'
+
+import NumericValue from '../numeric-value'
+import styles from './styles.css'
+
+const PerformanceChart = ({ value, increase, caption }) => (
+  <div className={styles.container}>
+    <div className={styles.rechartsWrapper}>
+      <ResponsiveContainer className={styles.rechartsInner}>
+        <RadialBarChart
+          innerRadius="100%"
+          data={[
+            {
+              value: Number(value) || 0,
+              fill: 'var(--gray-800)',
+            },
+            increase &&
+              Number(value + increase) > 0 &&
+              increase > 0 && {
+                value: value + increase,
+                fill: 'var(--primary)',
+              },
+            {
+              value: 100,
+              fill: '#fff',
+              background: { fill: 'transparent' },
+            },
+          ].filter(truthy => truthy)}
+          startAngle={225}
+          endAngle={-45}
+        >
+          <RadialBar
+            minAngle={15}
+            background={{ fill: 'var(--gray-200)' }}
+            clockWise
+            dataKey="value"
+            isAnimationActive={false}
+          />
+        </RadialBarChart>
+      </ResponsiveContainer>
+    </div>
+    <NumericValue
+      className={styles.label}
+      value={value}
+      append="%"
+      caption={caption}
+    />
+  </div>
+)
+
+export default PerformanceChart
