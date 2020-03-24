16db8e04c07b6966a3d1ee6764a531d736f7f066
Jozef Harag <32jojo32@gmail.com>
2020-03-24 16:16:44 +0100

    Return row to previous state when take down fails

--- store/works.js
+++ store/works.js
@@ -12,14 +12,19 @@ class Works extends Pages {
   @action
   async changeVisibility(rowId) {
     const [pageNumber, workId] = rowId.split('-', 2)
-    await apiRequest(`/works/${workId}`, {
-      method: 'PATCH',
-      body: { disabled: !isVisible },
-    })
-
     const page = this.getPageByNumber(parseInt(pageNumber, 10))
     const row = page.data.find(r => r.originalId === workId)
-    row.disabled = !row.disabled
+    const { disabled } = row
+    row.disabled = !disabled
+    try {
+      await apiRequest(`/works/${workId}`, {
+        method: 'PATCH',
+        body: { disabled: !disabled },
+      })
+    } catch (e) {
+      row.disabled = disabled
+      throw e
+    }
   }
 }
 
