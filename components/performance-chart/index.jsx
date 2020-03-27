1dd4454bdaa0402daf7a714aaee46cd1c4efc9a9
Viktor Yakubiv <viktor@yakubiv.com>
2020-03-27 17:58:57 +0200

    Add a second bar to the PerformanceChart
    Also, fixes chart spacing

--- components/performance-chart/index.jsx
+++ components/performance-chart/index.jsx
@@ -4,21 +4,25 @@ import { RadialBar, RadialBarChart, ResponsiveContainer } from 'recharts'
 import NumericValue from '../numeric-value'
 import styles from './styles.css'
 
-const PerformanceChart = ({ value, caption }) => (
-  <div className={styles.chartContainer}>
+const PerformanceChart = ({ value, increase, caption }) => (
+  <div className={styles.chartContainer} style={{ marginBottom: -64 + 24 }}>
     <ResponsiveContainer width="100%" height={320 - 64}>
       <RadialBarChart
-        innerRadius="95%"
+        innerRadius="100%"
         data={[
           {
             value,
+            fill: 'var(--gray-800)',
+          },
+          increase && {
+            value: value + increase,
             fill: 'var(--primary)',
           },
           {
             value: 100,
             fill: '#fff',
           },
-        ]}
+        ].filter(truthy => truthy)}
         startAngle={225}
         endAngle={-45}
       >
