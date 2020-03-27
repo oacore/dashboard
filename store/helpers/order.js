3c092d1403cf90bbb9133c5ea2b361aac1739d27
Viktor Yakubiv <viktor@yakubiv.com>
2020-03-27 12:42:12 +0200

    Remove extra } in request order param processing
    Fixes 500 error with invalid orderBy parameter

--- store/helpers/order.js
+++ store/helpers/order.js
@@ -2,7 +2,7 @@ const getOrder = columnOrder =>
   Object.entries(columnOrder)
     .map(([property, direction]) => [property, (direction || '').toLowerCase()])
     .filter(([, direction]) => direction === 'asc' || direction === 'desc')
-    .map(([property, direction]) => `${property}:${direction}}`)
+    .map(([property, direction]) => `${property}:${direction}`)
     .join(';')
 
 export default getOrder
