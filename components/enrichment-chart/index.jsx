799695492b7aecfe9b41bc64f8be051383f89d32
Viktor Yakubiv <viktor@yakubiv.com>
2020-03-27 18:01:12 +0200

    Develop EnrichmentChart component
    The chart is 2 horizontal bars:
    - first represents CORE value
    - second represents source value
    
    If `increase` parameter is not set, only source value
    is shown.
    
    If the source value is not number, it's treated being 0.

---
+++ components/enrichment-chart/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+  BarChart,
+  Bar,
+  LabelList,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from 'recharts'
+
+const VerticalBarChart = ({
+  data,
+  dataKey = 'value',
+  className,
+  width = '100%',
+  height = 128,
+  ...restProps
+}) => (
+  <ResponsiveContainer className={className} width={width} height={height}>
+    <BarChart data={data} layout="vertical" {...restProps}>
+      <XAxis type="number" hide />
+      <YAxis type="category" hide />
+      <Bar dataKey={dataKey} isAnimationActive={false}>
+        <LabelList
+          dataKey="caption"
+          position="insideLeft"
+          fill="#fff"
+          offset={16}
+        />
+        <LabelList
+          dataKey="value"
+          position="insideRight"
+          formatter={value => Number(value).toLocaleString('en-GB')}
+          fill="#fff"
+          offset={16}
+        />
+      </Bar>
+    </BarChart>
+  </ResponsiveContainer>
+)
+
+const EnrichmentChart = ({ value, caption, increase }) => {
+  const data = [
+    increase && {
+      caption: 'CORE',
+      value: value + increase || 0,
+      fill: 'var(--primary)',
+    },
+    {
+      caption,
+      value: Number(value) || 0,
+      fill: increase ? 'var(--gray-700)' : 'var(--primary)',
+    },
+  ].filter(truthy => truthy)
+
+  return <VerticalBarChart data={data} />
+}
+
+export default EnrichmentChart
