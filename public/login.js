a8080fab4ea70eb19e78cc501f8839f33d192351
Viktor Yakubiv <viktor@yakubiv.com>
2020-03-30 19:11:46 +0300

    Redirect users to the legacy app if access forbidden (#211)

--- public/login.js
+++ public/login.js
@@ -1,3 +1,22 @@
+/* eslint-disable max-classes-per-file */
+
+class UnauthorisedError extends Error {}
+class ForbiddenError extends Error {}
+
+function performLoginRequest(url, data) {
+  return fetch(url, {
+    method: 'POST',
+    body: data,
+    credentials: 'include',
+  }).then(response => {
+    if (response.status === 401)
+      throw new UnauthorisedError('Wrong credentials')
+    if (response.status === 402 || response.status === 403)
+      throw new ForbiddenError('User does not have access to this version')
+    return response
+  })
+}
+
 function addLoadingAnimation() {
   const form = document.getElementById('login-form')
   form.classList.add('loading')
@@ -31,23 +50,33 @@ function login(event) {
   const urlParams = new URLSearchParams(window.location.search)
   const identityProviderUrl =
     urlParams.get('identity_provider_url') || 'https://api.core.ac.uk/'
+  const fallbackIDP =
+    urlParams.get('fallback_idp') || 'https://dashboard.core.ac.uk/'
 
   window.dispatchEvent(new Event('login-processing'))
   const formData = new FormData(event.target)
   const data = new URLSearchParams(formData)
-  const url = new URL('login_check', identityProviderUrl)
-  fetch(url, {
-    method: 'POST',
-    body: data,
-    credentials: 'include',
-  })
-    .then(r => {
-      if (r.status === 401) showWrongCredentialsMessage()
-    })
-    .finally(() => {
+  const loginUrl = new URL('login_check', identityProviderUrl)
+  performLoginRequest(loginUrl, data)
+    .then(
+      () => 'login-processing',
+      error => {
+        if (error instanceof ForbiddenError) {
+          const fallbackUrl = new URL('login_check', fallbackIDP)
+          return performLoginRequest(fallbackUrl, data).then(
+            () => 'login-fallback',
+            () => 'login-fallback'
+          )
+        }
+
+        if (error instanceof UnauthorisedError) showWrongCredentialsMessage()
+        return 'login-processing'
+      }
+    )
+    .then(message => {
       if (window.location !== window.parent.location) {
         // The page is in an iframe
-        window.parent.postMessage('login-processing', window.location.origin)
+        window.parent.postMessage(message, window.location.origin)
       } else window.location = '/'
     })
 
