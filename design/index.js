ea7f9b5ee2e05277823858aff2c81f516572f730
Jozef Harag <32jojo32@gmail.com>
2020-04-02 12:54:07 +0200

    Remove unused code

--- design/index.js
+++ design/index.js
@@ -1,7 +1,6 @@
 export * from '@oacore/design/lib'
 
 export AppBarToggle from './app-bar/toggle'
-export Avatar from './avatar'
 export Card from './card'
 export CloseIcon from './close-icon'
 export Drawer from './drawer'
@@ -9,5 +8,4 @@ export Logo from './logo'
 export Numeral from './numeral'
 export Overlay from './overlay'
 export Select from './select'
-export Alert from './alert'
 export DetailList from './detail-list'
