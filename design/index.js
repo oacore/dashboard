2b2e225d5f4fa98cd6d8e777f1f262fd0c2492ab
Jozef Harag <32jojo32@gmail.com>
2020-04-14 13:45:18 +0200

    Use elements from @oacore/design (#255)
    * Bumps @oacore/design to 0.0.14
    
    * Replaces elements with @oacore/design ones:
      * AppBar
      * Card
      * DetailList
      * Table

--- design/index.js
+++ design/index.js
@@ -1,11 +1,9 @@
 export * from '@oacore/design/lib'
 
 export AppBarToggle from './app-bar/toggle'
-export Card from './card'
 export CloseIcon from './close-icon'
 export Drawer from './drawer'
 export Logo from './logo'
 export Numeral from './numeral'
 export Overlay from './overlay'
 export Select from './select'
-export DetailList from './detail-list'
