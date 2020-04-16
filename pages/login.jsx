9dadbe315651ac4b71a8a2f1ade7ae932a22e21b
Jozef Harag <32jojo32@gmail.com>
2020-04-16 16:42:12 +0200

    Use css from design in login

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useRef, useEffect, useCallback } from 'react'
 import { withRouter } from 'next/router'
 
 import styles from './login.css'
 
+import { Card } from 'design'
+
 const { IDP_URL } = process.env
 
 const Login = React.memo(({ router }) => {
+  const loginIframeRef = useRef(null)
   const searchParams = {
     reason: router.query.reason === 'logout' ? 'logout' : '',
     identity_provider_url: IDP_URL,
@@ -13,8 +16,27 @@ const Login = React.memo(({ router }) => {
 
   const search = new URLSearchParams(searchParams).toString()
 
+  const resize = useCallback(() => {
+    loginIframeRef.current.style.height = `${loginIframeRef.current.contentWindow.document.body.offsetHeight}px`
+  }, [])
+
+  useEffect(() => {
+    const observer = new ResizeObserver(resize)
+    observer.observe(loginIframeRef.current.contentWindow.document.body)
+    return () => observer.disconnect()
+  }, [])
+
   const url = `/secure/login.html?${search}`
-  return <iframe title="Login Form" src={url} className={styles.loginIframe} />
+  return (
+    <Card className={styles.loginIframeContainer}>
+      <iframe
+        ref={loginIframeRef}
+        title="Login Form"
+        src={url}
+        onLoad={resize}
+      />
+    </Card>
+  )
 })
 
 export default withRouter(Login)
