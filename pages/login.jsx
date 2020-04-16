3f59dad4c681c52338c9313c7d587e2e5a2efdcb
Jozef Harag <32jojo32@gmail.com>
2020-04-16 17:04:20 +0200

    Move iframe resize to separate component

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,14 +1,12 @@
-import React, { useRef, useEffect, useCallback } from 'react'
+import React from 'react'
 import { withRouter } from 'next/router'
 
 import styles from './login.css'
-
-import { Card } from 'design'
+import IframeForm from '../form/iframe-form'
 
 const { IDP_URL } = process.env
 
 const Login = React.memo(({ router }) => {
-  const loginIframeRef = useRef(null)
   const searchParams = {
     reason: router.query.reason === 'logout' ? 'logout' : '',
     identity_provider_url: IDP_URL,
@@ -16,26 +14,13 @@ const Login = React.memo(({ router }) => {
 
   const search = new URLSearchParams(searchParams).toString()
 
-  const resize = useCallback(() => {
-    loginIframeRef.current.style.height = `${loginIframeRef.current.contentWindow.document.body.offsetHeight}px`
-  }, [])
-
-  useEffect(() => {
-    const observer = new ResizeObserver(resize)
-    observer.observe(loginIframeRef.current.contentWindow.document.body)
-    return () => observer.disconnect()
-  }, [])
-
   const url = `/secure/login.html?${search}`
   return (
-    <Card className={styles.loginIframeContainer}>
-      <iframe
-        ref={loginIframeRef}
-        title="Login Form"
-        src={url}
-        onLoad={resize}
-      />
-    </Card>
+    <IframeForm
+      className={styles.loginIframeContainer}
+      title="Login Form"
+      src={url}
+    />
   )
 })
 
