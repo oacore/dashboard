a2d531462cb0bd663bd8fd91c3af280f455469a2
Jozef Harag <32jojo32@gmail.com>
2020-04-17 09:17:49 +0200

    Disable aggregation in time lag chart

--- components/time-lag-chart/index.jsx
+++ components/time-lag-chart/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Cell,
   Bar,
@@ -9,118 +9,62 @@ import {
   ResponsiveContainer,
 } from 'recharts'
 import { classNames } from '@oacore/design/lib/utils'
-import { nest } from 'd3-collection'
-import { sum, mean } from 'd3-array'
 
 import CustomTooltip from './tooltip'
 import styles from './index.module.css'
 
-const customTicks = {
-  ...Object.fromEntries(
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].flatMap((i) => [
-      [-i * 365, `${-i}y`],
-      [i * 365, `${i}y`],
-    ])
-  ),
-  7: '1w',
-  31: '1m',
-  90: '90d',
-}
-
-const aggregationSize = 14
-
-const isInInterval = (groupIndex, dayIndex, groupSize = aggregationSize) =>
-  dayIndex >= groupIndex * groupSize &&
-  dayIndex <= groupIndex * groupSize + groupSize - 1
-
-const isTick = (groupIndex) =>
-  Object.keys(customTicks).some((day) =>
-    isInInterval(groupIndex, parseInt(day, 10))
-  )
-
-const formatter = (g) => {
-  const groupIndex = parseInt(g, 10)
-  const el = Object.entries(customTicks).find((e) =>
-    isInInterval(groupIndex, parseInt(e[0], 10))
-  )
-  if (el) return el[1]
-  return ''
-}
-
 const TimeLagChart = React.memo(
   ({ data, width = '100%', height = 300, ...restProps }) => {
-    const [aggregatedData, setAggregatedData] = useState([])
+    const [normalizedData, setNormalizedData] = useState([])
 
     useEffect(() => {
       const rawIntervalSize =
         data[data.length - 1].depositTimeLag - data[0].depositTimeLag
       const dataMap = new Map(data.map((e) => [e.depositTimeLag, e.worksCount]))
-      const normalizedData = []
+      const normalize = []
 
-      for (
-        let i = 0;
-        i < rawIntervalSize + (rawIntervalSize % aggregationSize);
-        i++
-      ) {
+      for (let i = 0; i < rawIntervalSize; i++) {
         const lagIndex = i + data[0].depositTimeLag
         if (dataMap.has(lagIndex)) {
-          normalizedData.push({
+          normalize.push({
             depositTimeLag: lagIndex,
             worksCount: dataMap.get(lagIndex),
           })
         } else {
-          normalizedData.push({
+          normalize.push({
             depositTimeLag: lagIndex,
             worksCount: 0,
           })
         }
       }
 
-      const aggregation = nest()
-        .key((d) => Math.floor(d.depositTimeLag / aggregationSize))
-        .rollup((v) => ({
-          total: sum(v, (d) => d.worksCount),
-          avg: mean(v, (d) => d.worksCount),
-        }))
-        .entries(normalizedData)
-
-      setAggregatedData(aggregation)
+      setNormalizedData(normalize)
     }, [data])
 
-    const ticks = useMemo(
-      () =>
-        aggregatedData
-          .filter((e) => isTick(parseInt(e.key, 10)))
-          .map((e) => e.key),
-      [aggregatedData]
-    )
-
-    if (aggregatedData.length === 0) return null
+    if (normalizedData.length === 0) return null
 
     return (
       <ResponsiveContainer width={width} height={height} {...restProps}>
-        <BarChart margin={{ bottom: -5 }} data={aggregatedData}>
+        <BarChart margin={{ bottom: -5 }} data={normalizedData}>
           <XAxis
-            dataKey="key"
+            dataKey="depositTimeLag"
             tickLine={false}
-            ticks={ticks}
-            tickFormatter={formatter}
+            // TODO: create function for generating these values
+            ticks={[0, 30, 92]}
           />
-          <Tooltip
-            content={<CustomTooltip aggregationSize={aggregationSize} />}
-          />
+          <Tooltip content={<CustomTooltip />} />
           <ReferenceLine y={0} className={styles.referenceLine} />
-          <Bar dataKey="value.total">
-            {aggregatedData.map((entry) => (
+          <Bar dataKey="worksCount">
+            {normalizedData.map(({ depositTimeLag }) => (
               <Cell
                 className={classNames
                   .use({
                     'lag-bar': true,
-                    'compliant': parseInt(entry.key, 10) * aggregationSize < 90,
+                    'compliant': parseInt(depositTimeLag, 10) < 90,
                   })
                   .from(styles)
                   .toString()}
-                key={entry.key}
+                key={depositTimeLag}
               />
             ))}
           </Bar>
--- components/time-lag-chart/tooltip.jsx
+++ components/time-lag-chart/tooltip.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 import styles from './index.module.css'
 
-const Tooltip = ({ active, payload, label, aggregationSize }) => {
-  const index = parseInt(label, 10) * aggregationSize
+const Tooltip = ({ active, payload, label }) => {
+  const index = parseInt(label, 10)
   if (!active) return null
 
   return (
     <div className={styles.tooltip}>
-      {Math.abs(payload[0].value)} works deposited in <br />
-      {Math.abs(index)} - {Math.abs(index + aggregationSize - 1)} days{' '}
-      {index < 0 ? 'before' : 'after'} publication
+      {Math.abs(payload[0].value)} works deposited <br />
+      {Math.abs(index)} days {index < 0 ? 'before' : 'after'} publication
     </div>
   )
 }
