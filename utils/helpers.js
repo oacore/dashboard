960c820bdbf0a7dcc9c07ee28c246cc2a7c28057
Jozef Harag <32jojo32@gmail.com>
2020-04-23 15:48:55 +0200

    Format pagination properly

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,9 @@
 export const range = (length, start = 0, step = 1) =>
   Array.from(Array(length), (_, i) => start + i * step)
 
+export const formatNumber = (number) =>
+  new Intl.NumberFormat('en-GB', { maximumSignificantDigits: 3 }).format(number)
+
 export default {
   range,
 }
