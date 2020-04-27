ec9257a03ca58e7027247d582d3a7e34b171ffa2
Jozef Harag <32jojo32@gmail.com>
2020-04-27 14:24:14 +0200

    Show message when email has already been invited

--- api/errors.js
+++ api/errors.js
@@ -13,12 +13,14 @@ export class ForbiddenError extends NetworkError {}
 export class NotFoundError extends NetworkError {}
 export class BadRequestError extends NetworkError {}
 export class InternalServerError extends NetworkError {}
+export class NotAcceptableError extends NetworkError {}
 
 NetworkError.getErrorFromStatusCode = (statusCode) => {
   if (statusCode === 401) return UnauthorizedError
   if (statusCode === 401) return PaymentRequiredError
   if (statusCode === 403) return ForbiddenError
   if (statusCode === 404) return NotFoundError
+  if (statusCode === 406) return NotAcceptableError
   if (statusCode >= 400 && statusCode < 500) return BadRequestError
   if (statusCode >= 500) return InternalServerError
   return NetworkError
--- store/organisation.js
+++ store/organisation.js
@@ -1,15 +1,23 @@
 import { action } from 'mobx'
 
 import Store from './store'
+import { NotAcceptableError } from '../api/errors'
 
 class Organisation extends Store {
   @action
   inviteUser = async (data) => {
     const url = `${this.url}/invitation`
-    await this.request(url, {
-      method: 'POST',
-      body: data,
-    })
+    try {
+      await this.request(url, {
+        method: 'POST',
+        body: data,
+      })
+    } catch (error) {
+      if (error instanceof NotAcceptableError)
+        return { message: 'Email for this organisation is already registered.' }
+    }
+
+    return { message: 'Invitation has been sent.' }
   }
 }
 
