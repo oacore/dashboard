89f6580ea29d7336e430091e5523bf89728afb2d
Viktor Yakubiv <viktor@yakubiv.com>
2020-04-29 17:38:16 +0300

    WIP

--- components/time-lag-chart/index.jsx
+++ components/time-lag-chart/index.jsx
@@ -1,131 +1,91 @@
+/* eslint-disable */
 import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
 import { Cell, Bar, BarChart, ReferenceLine, XAxis, Tooltip } from 'recharts'
 import { classNames } from '@oacore/design/lib/utils'
 import { scaleSqrt } from 'd3-scale'
 
+import depositTimeLag from './data'
 import CustomTooltip from './tooltip'
 import styles from './styles.module.css'
 
-const TimeLagChart = React.memo(
-  ({
-    data,
-    className,
-    from = Number.NEGATIVE_INFINITY,
-    size = Number.POSITIVE_INFINITY,
-    height = 300,
-    barWidth = 4,
-    gutterWidth = 2,
-    ...restProps
-  }) => {
-    const [normalizedData, setNormalizedData] = useState([])
+const COMPLIANCE_LIMIT = 90
 
-    const chartRef = useRef(null)
-    useEffect(() => {
-      const rawIntervalSize =
-        data[data.length - 1].depositTimeLag - data[0].depositTimeLag
-      const dataMap = new Map(data.map((e) => [e.depositTimeLag, e.worksCount]))
-      const normalize = []
+// converts source DepositTimeLag object to simple [x, y] point
+const toPoints = ({ depositTimeLag, worksCount }) => [
+  depositTimeLag,
+  worksCount,
+]
 
-      const maxNumber = data.reduce(
-        (acc, curr) => Math.max(curr.worksCount, acc),
-        1
-      )
-      const scale = scaleSqrt().domain([0, maxNumber])
+// converts [x, y] point to actual value
+const toX = ([x]) => x
+const toY = ([, y]) => y
 
-      for (let i = 0; i <= rawIntervalSize; i++) {
-        const lagIndex = i + data[0].depositTimeLag
-        if (lagIndex >= from && normalize.length < size) {
-          normalize.push({
-            depositTimeLag: lagIndex,
-            worksCount: dataMap.get(lagIndex) || 0,
-            worksCountScaled: scale(dataMap.get(lagIndex) || 0),
-          })
-        }
-      }
+// creates square root scale applicator based on maximal value parameter
+const toSqrtScale = (yMax) => {
+  const scale = scaleSqrt().domain([0, yMax])
+  return ([x, y]) => [x, scale(y || 0)]
+}
 
-      setNormalizedData(normalize)
-    }, [data])
+/**
+ * For a sorted array of [x, y] pints returns new array,
+ * where are missing x-values filled with y-values = 0
+ */
+const withGaps = (values = [], [x, y]) => {
+  const [prevX] = values[values.length - 1] ?? [x - 1]
+  values.push(...Array.from(Array(x - prevX - 1), (_, i) => [prevX + i + 1, 0]))
+  values.push([x, y])
+  return values
+}
 
-    const callback = useCallback((mutationsList, observer) => {
-      mutationsList.forEach((mutation) => {
-        if (mutation.type === 'childList') {
-          mutation.addedNodes.forEach((el) => {
-            if (
-              el.classList.contains('start') ||
-              // sometimes mutation observer detect only change in parent,
-              // i.e. g element not path element
-              el.firstElementChild?.classList.contains('start')
-            ) {
-              el.scrollIntoView({
-                // alight horizontally
-                inline: 'start',
-              })
-              observer.disconnect()
-            }
-          })
-        }
-      })
-    }, [])
-
-    useEffect(() => {
-      const observer = new MutationObserver(callback)
-      const config = {
-        childList: true,
-        subtree: true,
-      }
-      observer.observe(chartRef.current, config)
-      chartRef.current.style.setProperty('--chart-height', `${height}px`)
-
-      return () => {
-        observer.disconnect()
-      }
-    }, [])
-
-    // if bar is empty recharts doesn't render cell
-    const barWithValue = useMemo(
-      () =>
-        normalizedData?.find((d) => d.depositTimeLag >= -20 && d.worksCount),
-      [normalizedData]
-    )
-
+const TimeLagChart = React.forwardRef(
+  (
+    {
+      className,
+      data,
+      barWidth = 4,
+      gutterWidth = 2,
+      height = 300,
+      ...restProps
+    },
+    ref
+  ) => {
+    const chartRef = useRef(ref)
     return (
       <div
         ref={chartRef}
         className={classNames.use(styles.chartWrapper, className)}
         {...restProps}
       >
-        {normalizedData.length !== 0 && (
+        {data.length !== 0 && (
           <BarChart
             // let's assume approx. 6 (4 bar width + 2 bar gap) pixels
             // per bar by default
-            width={normalizedData.length * (barWidth + gutterWidth)}
+            width={data.length * (barWidth + gutterWidth)}
             height={height}
-            data={normalizedData}
+            data={data}
             barCategoryGap={gutterWidth}
             barSize={barWidth}
           >
             <XAxis
-              dataKey="depositTimeLag"
+              dataKey="0"
               tickLine={false}
-              ticks={normalizedData
-                .map((d) => d.depositTimeLag)
-                .filter((i) => i % 30 === 0)}
+              ticks={data.map(toX).filter((x) => x % 30 === 0)}
             />
-            <Tooltip content={<CustomTooltip data={normalizedData} />} />
+            <Tooltip content={<CustomTooltip data={data} />} />
             <ReferenceLine y={0} className={styles.referenceLine} />
-            <Bar dataKey="worksCountScaled">
-              {normalizedData.map(({ depositTimeLag }) => (
+            <Bar dataKey="1">
+              {data.map(([x]) => (
                 <Cell
-                  id={depositTimeLag}
+                  id={x}
                   className={classNames
                     .use({
                       'lag-bar': true,
-                      'compliant': depositTimeLag < 90,
-                      'start': depositTimeLag === barWithValue.depositTimeLag,
+                      'compliant': x < COMPLIANCE_LIMIT,
+                      'start': x === 0,
                     })
                     .from(styles)
                     .toString()}
-                  key={depositTimeLag}
+                  key={x}
                 />
               ))}
             </Bar>
@@ -136,4 +96,90 @@ const TimeLagChart = React.memo(
   }
 )
 
-export default TimeLagChart
+const TimeLagChartController = React.memo(
+  ({
+    data,
+    from = Number.NEGATIVE_INFINITY,
+    size = Number.POSITIVE_INFINITY,
+    ...restProps
+  }) => {
+    const points = depositTimeLag.map(toPoints)
+    const scaledPoints = points
+      .map(toSqrtScale(Math.max(...points.map(toY))))
+      .reduce(withGaps)
+    console.log(scaledPoints)
+
+    //     const chartRef = useRef(null)
+    //     useEffect(() => {
+    //       const rawIntervalSize =
+    //         data[data.length - 1].depositTimeLag - data[0].depositTimeLag
+    //       const dataMap = new Map(data.map((e) => [e.depositTimeLag, e.worksCount]))
+    //       const normalize = []
+    //
+    //       const maxNumber = data.reduce(
+    //         (acc, curr) => Math.max(curr.worksCount, acc),
+    //         1
+    //       )
+    //       const scale = scaleSqrt().domain([0, maxNumber])
+    //
+    //       for (let i = 0; i <= rawIntervalSize; i++) {
+    //         const lagIndex = i + data[0].depositTimeLag
+    //         if (lagIndex >= from && normalize.length < size) {
+    //           normalize.push({
+    //             depositTimeLag: lagIndex,
+    //             worksCount: dataMap.get(lagIndex) || 0,
+    //             worksCountScaled: scale(dataMap.get(lagIndex) || 0),
+    //           })
+    //         }
+    //       }
+    //
+    //       setNormalizedData(normalize)
+    //     }, [data])
+
+    //     const callback = useCallback((mutationsList, observer) => {
+    //       mutationsList.forEach((mutation) => {
+    //         if (mutation.type === 'childList') {
+    //           mutation.addedNodes.forEach((el) => {
+    //             if (
+    //               el.classList.contains('start') ||
+    //               // sometimes mutation observer detect only change in parent,
+    //               // i.e. g element not path element
+    //               el.firstElementChild?.classList.contains('start')
+    //             ) {
+    //               el.scrollIntoView({
+    //                 // alight horizontally
+    //                 inline: 'start',
+    //               })
+    //               observer.disconnect()
+    //             }
+    //           })
+    //         }
+    //       })
+    //     }, [])
+    //
+    //     useEffect(() => {
+    //       const observer = new MutationObserver(callback)
+    //       const config = {
+    //         childList: true,
+    //         subtree: true,
+    //       }
+    //       observer.observe(chartRef.current, config)
+    //       chartRef.current.style.setProperty('--chart-height', `${height}px`)
+    //
+    //       return () => {
+    //         observer.disconnect()
+    //       }
+    //     }, [])
+
+    // if bar is empty recharts doesn't render cell
+    // const barWithValue = useMemo(
+    //   () =>
+    //     normalizedData?.find((d) => d.depositTimeLag >= -20 && d.worksCount),
+    //   [normalizedData]
+    // )
+
+    return <TimeLagChart data={scaledPoints} {...restProps} />
+  }
+)
+
+export default TimeLagChartController
