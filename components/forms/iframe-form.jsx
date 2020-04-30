bb98e702353f92e33617f20917c91f4a96b0c0f4
Jozef Harag <32jojo32@gmail.com>
2020-04-30 09:49:56 +0200

    Observer iframe after ref is assigned
    React guarantees that ref is assigned before effect is called only when ref is direct child.

--- components/forms/iframe-form.jsx
+++ components/forms/iframe-form.jsx
@@ -10,10 +10,12 @@ const IframeForm = ({ className, title, ...passProps }) => {
   }, [])
 
   useEffect(() => {
+    if (ref.current === null) return () => {}
+
     const observer = new ResizeObserver(resize)
     observer.observe(ref.current.contentWindow.document.body)
     return () => observer.disconnect()
-  }, [])
+  }, [ref.current])
 
   return (
     <Card className={className}>
