a77e31afb0862d3946938b88839e769e0d6608fd
Viktor Yakubiv <viktor@yakubiv.com>
2020-05-08 21:43:34 +0300

    Update descriptions of the issues (#321)
    Copies new issues from the document, adds a reviewed texts.
    
    Adds `matches` property to support future renaming of the enumeration
    keys.

--- texts/issues/index.js
+++ texts/issues/index.js
@@ -2,8 +2,14 @@ import sourceMessages from './messages.yml'
 import issuesTab from './issues.yml'
 
 const messages = {}
+
+// Object.values() because YAML loader for some reason
+// returns objects instead arrays
 Object.values(sourceMessages).forEach((msg) => {
   messages[msg.type] = msg
+  Object.values(msg.matches ?? {}).forEach((type) => {
+    messages[type] = msg
+  })
 })
 
 export default {
