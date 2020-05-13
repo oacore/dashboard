ea9ef24503f14621b421924730411c08822b2940
Viktor Yakubiv <viktor@yakubiv.com>
2020-05-13 15:50:39 +0300

    Add `matches` support

--- texts/issues/index.js
+++ texts/issues/index.js
@@ -4,6 +4,9 @@ import issuesTab from './issues.yml'
 const messages = {}
 Object.values(sourceMessages).forEach((msg) => {
   messages[msg.type] = msg
+  ;(msg.matches ?? []).forEach((type) => {
+    messages[type] = msg
+  })
 })
 
 export default {
