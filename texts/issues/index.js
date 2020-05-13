7ffd952195a7552c79ba6c6a0af11d089bc73d3c
Viktor Yakubiv <viktor@yakubiv.com>
2020-05-13 16:10:04 +0300

    Fix parsing and interpretting errors

--- texts/issues/index.js
+++ texts/issues/index.js
@@ -2,9 +2,12 @@ import sourceMessages from './messages.yml'
 import issuesTab from './issues.yml'
 
 const messages = {}
+
+// Object.values() because YAML loader for some reason
+// returns objects instead arrays
 Object.values(sourceMessages).forEach((msg) => {
   messages[msg.type] = msg
-  ;(msg.matches ?? []).forEach((type) => {
+  Object.values(msg.matches ?? {}).forEach((type) => {
     messages[type] = msg
   })
 })
