83a944c9e6aa60d70a5cda02c872a168f2e0c907
Viktor Yakubiv <viktor@yakubiv.com>
2020-06-02 15:32:25 +0300

    Fix 404 processing for paginated requests

--- store/helpers/pages/pages.js
+++ store/helpers/pages/pages.js
@@ -82,18 +82,19 @@ class Pages extends Store {
           resolve(transformedData)
         })
         .catch((error) => {
-          // Storing the error to propagate messages to the UI
-          this.error = error
-          this.data = error.data
-
           // Resetting pointers to prevent pagination working
           this.isFirstPageLoaded = true
           this.isLastPageLoaded = true
 
-          if (error instanceof NotFoundError) {
-            this.data = []
-            resolve()
-          } else if (error instanceof PaymentRequiredError) {
+          // We use NotFoundError to detect the end of the data stream
+          // If it happens we avoid any error processing
+          if (error instanceof NotFoundError) return resolve()
+
+          // Storing the error to propagate messages to the UI
+          this.error = error
+          this.data = error.data
+
+          if (error instanceof PaymentRequiredError) {
             // Stripping data to 5 elements only if there are more
             // These elements may be used as a preview
             this.data = this.data.slice(0, 5)
@@ -103,8 +104,10 @@ class Pages extends Store {
             // no data instead
             if (this.data.length === 0) this.error = null
 
-            resolve()
-          } else reject(error)
+            return resolve()
+          }
+
+          return reject(error)
         })
     )
   }
