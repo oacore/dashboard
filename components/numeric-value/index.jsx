e4c92fa05058d11fe2a1353d579966ed27ed3b8f
Viktor Yakubiv <viktor@yakubiv.com>
2020-06-03 15:55:49 +0300

    Add more options to NumericValue
    Uses default formatNumber() function.
    
    Allows to use any notation but keep 'compact' as default.

--- components/numeric-value/index.jsx
+++ components/numeric-value/index.jsx
@@ -1,35 +1,22 @@
 import React from 'react'
 
 import { Numeral } from 'design'
+import { formatNumber } from 'utils/helpers'
 
-const formatNumber = (
-  number,
-  {
-    locale = 'en-GB',
-    precision = 2,
-    spacer = '\xa0', // tiny space, smaller then regular
-  } = {}
-) => {
-  const divisionMap = [
-    [1e9, 'B'],
-    [1e6, 'M'],
-    [1e3, 'K'],
-  ]
-
-  const [divider, appendix] = divisionMap.find(
-    ([rank]) => Math.abs(Math.floor(number / rank)) > 0
-  ) ?? [1, '']
-  const actualNumber =
-    Math.floor((number / divider) * 10 ** precision) / 10 ** precision
-  return [actualNumber.toLocaleString(locale), appendix]
-    .filter((x) => x) // drop empty appendix
-    .join(spacer)
-}
-
-const NumericValue = ({ value, append, caption, diff, ...restProps }) => (
-  <Numeral {...restProps}>
+const NumericValue = ({
+  value,
+  append,
+  caption,
+  diff,
+  notation = 'compact',
+  compactDisplay,
+  ...htmlProps
+}) => (
+  <Numeral {...htmlProps}>
     <Numeral.Value>
-      {typeof value == 'number' ? formatNumber(value) : value}
+      {typeof value == 'number'
+        ? formatNumber(value, { notation, compactDisplay })
+        : value}
     </Numeral.Value>
     {append && <Numeral.Appendix>{append}</Numeral.Appendix>}{' '}
     {diff && <Numeral.Diff>{diff}</Numeral.Diff>}{' '}
