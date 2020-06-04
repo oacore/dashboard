4ee097ecde58d7b4522256fd313ea155a0e42686
Viktor Yakubiv <viktor@yakubiv.com>
2020-06-04 17:14:30 +0300

    Rename 'IRUS' to 'Download statistics'
    Adds an explanation of the statistics collecting process.

--- templates/overview/cards/irus-card.jsx
+++ templates/overview/cards/irus-card.jsx
@@ -4,6 +4,7 @@ import OverviewCard from './overview-card'
 import styles from '../styles.module.css'
 
 import { Card } from 'design'
+import Markdown from 'components/markdown'
 import StatisticsChart from 'components/statistics-chart'
 import { formatDate } from 'utils/helpers'
 
@@ -19,12 +20,16 @@ const Content = ({ data, ...htmlProps }) => (
         'Views in CORE': 'var(--primary)',
       }}
     />
+    <Markdown>
+      Statistics collected by [IRUS](https://irus.jisc.ac.uk) from
+      [core.ac.uk](https://core.ac.uk)
+    </Markdown>
   </div>
 )
 
 const IrusCard = ({ statistics }) => (
   <OverviewCard>
-    <Card.Title tag="h2">IRUS</Card.Title>
+    <Card.Title tag="h2">Download statistics</Card.Title>
     {statistics != null ? (
       <Content className={styles.irusCardContent} data={statistics} />
     ) : (
