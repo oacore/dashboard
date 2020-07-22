d7978b1459381cf91217084dc168121efd8df5d0
Jozef Harag <32jojo32@gmail.com>
2020-07-22 13:24:45 +0200

    Use Logo component from design

--- design/index.js
+++ design/index.js
@@ -2,7 +2,6 @@ export * from '@oacore/design/lib'
 
 export AppBarToggle from './app-bar/toggle'
 export Drawer from './drawer'
-export Logo from './logo'
 export Numeral from './numeral'
 export Overlay from './overlay'
 export Select from './select'
