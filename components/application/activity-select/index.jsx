2207c8f745a9f8bdf16415c0cb9d1490d9e91922
Jozef Harag <32jojo32@gmail.com>
2020-09-14 10:39:46 +0200

    Fix data provider undefined when accessed from router query
    Happens for wrong URLs like:
    
    ```
    /data-providers/136/deposit-compliance),
    ```

--- components/application/activity-select/index.jsx
+++ components/application/activity-select/index.jsx
@@ -2,7 +2,6 @@ import path from 'path'
 
 import React from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 
 import activities from '../activities'
 import styles from './styles.module.css'
@@ -16,34 +15,36 @@ const ActivitySelect = ({ children }) => (
   <Drawer.List className={styles.list}>{children}</Drawer.List>
 )
 
-const ActivitySelectOption = ({ children, value, selected }) => {
-  const router = useRouter()
-  return (
-    <Link
-      href={path.join('/data-providers/[data-provider-id]', value)}
-      as={path.join('/data-providers', router.query['data-provider-id'], value)}
-      passHref
+const ActivitySelectOption = ({
+  children,
+  value,
+  selected,
+  dataProviderId,
+}) => (
+  <Link
+    href={path.join('/data-providers/[data-provider-id]', value)}
+    as={path.join('/data-providers', String(dataProviderId), value)}
+    passHref
+  >
+    <Drawer.Item
+      className={styles[value]}
+      active={selected}
+      title={navigation.tooltips[value]}
     >
-      <Drawer.Item
-        className={styles[value]}
-        active={selected}
-        title={navigation.tooltips[value]}
-      >
-        {children || (
-          <>
-            <Icon
-              className={styles.itemIcon}
-              src={toIcon(value)}
-              alt=""
-              aria-hidden
-            />
-            {navigation.items[value]}
-          </>
-        )}
-      </Drawer.Item>
-    </Link>
-  )
-}
+      {children || (
+        <>
+          <Icon
+            className={styles.itemIcon}
+            src={toIcon(value)}
+            alt=""
+            aria-hidden
+          />
+          {navigation.items[value]}
+        </>
+      )}
+    </Drawer.Item>
+  </Link>
+)
 
 ActivitySelect.Option = ActivitySelectOption
 
