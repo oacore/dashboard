f47009c354e70cf0260319c8071f301ca3f5535b
Jozef Harag <32jojo32@gmail.com>
2020-09-23 08:42:35 +0200

    Make application observer
    The reason for it is to rerender activities menu once the data provider is loaded. This bug affects only superadmin user because activities menu were rendered without data provider id, i.e. link looked like href="/data-providers/deposit-dates" because initial value of unknown data provider is empty string.

--- components/application/index.jsx
+++ components/application/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { observer } from 'mobx-react-lite'
 
 import { Container, AppBar, SideBar, Main } from '../layout'
 import activities from './activities'
@@ -8,48 +9,50 @@ import ActivitySelect from './activity-select'
 import Head from './head'
 import Logout from './logout'
 
-const Application = ({
-  children,
-  dataProvider,
-  pathname,
-  variant = 'public', // 'public' or 'internal'
-  isAuthenticated = false,
-  ...restProps
-}) => (
-  <>
-    <Head />
-    <Container variant={variant} {...restProps}>
-      <LoadingBar fixed />
-      <AppBar>
-        {isAuthenticated ? (
-          <>
-            {dataProvider && <RepositorySelect value={dataProvider} />}
-            <Logout />
-          </>
-        ) : null}
-      </AppBar>
-      {variant === 'internal' && (
-        <SideBar tag="nav">
-          <h2 className="sr-only">Navigate your data</h2>
-          {dataProvider && (
-            <ActivitySelect>
-              {activities.routes
-                .filter((activity) => activity.parent == null)
-                .map(({ path, test }) => (
-                  <ActivitySelect.Option
-                    key={path}
-                    value={path}
-                    selected={test(pathname)}
-                    dataProviderId={dataProvider.id}
-                  />
-                ))}
-            </ActivitySelect>
-          )}
-        </SideBar>
-      )}
-      {children && <Main>{children}</Main>}
-    </Container>
-  </>
+const Application = observer(
+  ({
+    children,
+    dataProvider,
+    pathname,
+    variant = 'public', // 'public' or 'internal'
+    isAuthenticated = false,
+    ...restProps
+  }) => (
+    <>
+      <Head />
+      <Container variant={variant} {...restProps}>
+        <LoadingBar fixed />
+        <AppBar>
+          {isAuthenticated ? (
+            <>
+              {dataProvider && <RepositorySelect value={dataProvider} />}
+              <Logout />
+            </>
+          ) : null}
+        </AppBar>
+        {variant === 'internal' && (
+          <SideBar tag="nav">
+            <h2 className="sr-only">Navigate your data</h2>
+            {Boolean(dataProvider.id) && (
+              <ActivitySelect>
+                {activities.routes
+                  .filter((activity) => activity.parent == null)
+                  .map(({ path, test }) => (
+                    <ActivitySelect.Option
+                      key={path}
+                      value={path}
+                      selected={test(pathname)}
+                      dataProviderId={dataProvider.id}
+                    />
+                  ))}
+              </ActivitySelect>
+            )}
+          </SideBar>
+        )}
+        {children && <Main>{children}</Main>}
+      </Container>
+    </>
+  )
 )
 
 export default Application
