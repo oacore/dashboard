275197d7f2f0b4597108a3564a43757df05f4fe1
Viktor Yakubiv <viktor@yakubiv.com>
2020-10-07 18:25:08 +0300

    Redesign Issues (#500)
    The issue list stops being a table and becomes a stack of cards.
    
    Each card can show the list of affected documents on request.
    The implementation is based on Pages and a React hook.

--- pages/data-providers/[data-provider-id]/issues.jsx
+++ pages/data-providers/[data-provider-id]/issues.jsx
@@ -7,7 +7,7 @@ const Issues = ({ store: { dataProvider }, ...props }) => (
   <IssuesTemplate
     harvestingStatus={dataProvider?.issues?.harvestingStatus}
     aggregation={dataProvider?.issues?.aggregation}
-    pages={dataProvider?.issues?.issues}
+    issuesByType={dataProvider?.issues?.issuesByType}
     {...props}
   />
 )
