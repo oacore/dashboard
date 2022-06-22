1325c5f1a1b7e48324eba768c6c3b57cbd6220d9
Kostiantyn Voskoboinik <k.voskoboinik99@gmail.com>
2022-06-22 09:04:38 +0300

    Fix problem with infinity loader

--- templates/overview/cards/data-statictics-card.jsx
+++ templates/overview/cards/data-statictics-card.jsx
@@ -7,7 +7,6 @@ import OverviewCard from './overview-card'
 import AreaChart from 'components/area-chart'
 import useHarvestingDate from 'utils/hooks/use-harvesting-dates'
 import { valueOrDefault, formatDate } from 'utils/helpers'
-import Loader from 'components/loader'
 import { Card } from 'design'
 import NumericValue from 'components/numeric-value'
 import text from 'texts/harvesting'
@@ -96,48 +95,47 @@ const DataStatisticsCard = ({
     <OverviewCard className={styles.infoCard} {...restProps}>
       <div className={styles.cardHeader}>
         <Card.Title tag="h2">{text.title}</Card.Title>
-        <ActionsBar activeType={activeType} onSetActiveType={onSetActiveType} />
+        {barChartValues.length > 5 && (
+          <ActionsBar
+            activeType={activeType}
+            onSetActiveType={onSetActiveType}
+          />
+        )}
       </div>
-
-      {!metadataCount || !fullTextCount ? (
-        <Loader width={100} />
+      <div className={styles.infoCardContent}>
+        <div className={styles.infoBox}>
+          <Markdown
+            className={classNames.use(styles.subtitle, styles.metadata)}
+          >
+            {text.metadata.title}
+          </Markdown>
+          <NumericValue
+            bold
+            tag="p"
+            value={valueOrDefault(metadataCount, 'Loading...')}
+          />
+        </div>
+        {fullTextCount && (
+          <FullTextsProgressChart
+            fullTextCount={fullTextCount}
+            chartValues={perfomanceChartValues}
+            caption={text.metadata.caption}
+            value={valueOrDefault((fullTextCount / metadataCount) * 100, '🔁')}
+          />
+        )}
+      </div>
+      {barChartValues.length > 5 ? (
+        <AreaChart
+          data={barChartValues.map(({ date, value }) => ({
+            'name': formatDate(date, {
+              month: 'short',
+              year: '2-digit',
+            }),
+            'Metadata count': value,
+          }))}
+        />
       ) : (
-        <>
-          <div className={styles.infoCardContent}>
-            <div className={styles.infoBox}>
-              <Markdown
-                className={classNames.use(styles.subtitle, styles.metadata)}
-              >
-                {text.metadata.title}
-              </Markdown>
-              <NumericValue
-                bold
-                tag="p"
-                value={valueOrDefault(metadataCount, 'Loading...')}
-              />
-            </div>
-            <FullTextsProgressChart
-              fullTextCount={fullTextCount}
-              chartValues={perfomanceChartValues}
-              caption={text.metadata.caption}
-              value={valueOrDefault(
-                (fullTextCount / metadataCount) * 100,
-                '🔁'
-              )}
-            />
-          </div>
-          {barChartValues.length > 0 && (
-            <AreaChart
-              data={barChartValues.map(({ date, value }) => ({
-                'name': formatDate(date, {
-                  month: 'short',
-                  year: '2-digit',
-                }),
-                'Metadata count': value,
-              }))}
-            />
-          )}
-        </>
+        <p>No compliance-aggregate for repository</p>
       )}
     </OverviewCard>
   )
