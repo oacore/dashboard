63b0685deeb6228d81d30be62328bace42e8da1f
admin <valerij.budko@gmail.com>
2023-01-25 11:36:28 +0000

    [bugfix] Babel runtime

--- components/forms/change-password/index.jsx
+++ components/forms/change-password/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import styles from './styles.module.css'
 
+// eslint-disable-next-line import/no-cycle
 import { IframeForm } from 'components/forms'
 import { Card } from 'design'
 
