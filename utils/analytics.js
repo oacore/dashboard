54ad6fb7b802c894191f0b288d31758084cff550
Budko <valerij.budko@gmail.com>
2023-05-29 15:20:08 +0100

    GA4 Init

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,10 +1,11 @@
-import { initialize, set, pageview } from 'react-ga'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import ReactGA from 'react-ga4'
 
 const isProduction = process.env.NODE_ENV === 'production'
 let isGAInitialized = false
 
 if (isProduction && process.env.GA_TRACKING_CODE) {
-  initialize(process.env.GA_TRACKING_CODE, {
+  ReactGA.initialize(process.env.GA_TRACKING_CODE, {
     siteSpeedSampleRate: 100,
   })
   isGAInitialized = true
@@ -12,8 +13,11 @@ if (isProduction && process.env.GA_TRACKING_CODE) {
 
 export const logPageView = (url = null) => {
   if (!isGAInitialized) return
-  set({ page: url || window.location.pathname })
-  pageview(url || window.location.pathname)
+  ReactGA.send({
+    hitType: 'pageview',
+    page: url || window.location.pathname,
+    title: window.location.pathname,
+  })
 }
 
 export default {
