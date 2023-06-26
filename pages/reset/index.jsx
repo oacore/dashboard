bca599e81f80eb8a63d0b68bbb671572102a8a26
Valeriy Budko <valerij.budko@gmail.com>
2023-06-26 16:37:54 +0100

    Percent fix (#630)

--- pages/reset/index.jsx
+++ pages/reset/index.jsx
@@ -19,7 +19,9 @@ const ResetPassword = ({ className, store, ...restProps }) => {
         email: formData.get('email'),
       }
       await store.requestResetToken(data)
-      setMessage('We have just sent you an email with the instructions on how to reset your password. Please check your mailbox and follow the reset link.')
+      setMessage(
+        'We have just sent you an email with the instructions on how to reset your password. Please check your mailbox and follow the reset link.'
+      )
     } catch (error) {
       setMessage('Something went wrong. Please try it again!')
     }
