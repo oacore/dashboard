a9a4b78d937c2a5493d4863aa8572440b7786901
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-12-20 15:28:48 +0400

    Core 4621 users in dashboard (#652)
    * CORE-4621 users ui
    
    * CORE-4621 api dataset users
    
    * CORE-4621 component placement
    
    * CORE-4621: toggle users

--- templates/settings/accessUsers.jsx
+++ templates/settings/accessUsers.jsx
@@ -1,49 +1,61 @@
 import { classNames } from '@oacore/design/lib/utils'
 import React from 'react'
+import { Button } from '@oacore/design/lib/elements'
 
 import { Card } from '../../design'
 import styles from './styles.module.css'
-import Markdown from '../../components/markdown'
 
 const AccessUsers = ({
   className,
   title,
   subTitle,
-  userNames,
-  userEmail,
   subDescription,
-  description,
-}) => (
-  <Card
-    className={classNames.use(styles.section).join(className)}
-    tag="section"
-  >
-    <div className={styles.formWrapper}>
-      <div className={styles.formInnerWrapper}>
-        <Card.Title tag="h2">{title}</Card.Title>
-        <Markdown className={styles.info}>{subTitle}</Markdown>
-        <div className={styles.userMainWrapper}>
-          <div className={styles.userWrapper}>
-            <div className={styles.user}>
-              <div className={styles.userName}>{userNames}</div>
-              <div className={styles.userName}>{userEmail}</div>
-            </div>
-            <p className={styles.subTitle}>{subDescription}</p>
-            <div className={styles.additionalInfo}>{description}</div>
-          </div>
-          <div className={styles.userWrapper}>
-            <div className={styles.user}>
-              <div className={styles.userName}>{userNames}</div>
-              <div className={styles.userName}>{userEmail}</div>
-            </div>
-            <p className={styles.subTitle}>{subDescription}</p>
-            <div className={styles.additionalInfo}>{description}</div>
+  userData,
+  toggleShowFullList,
+  showFullList,
+}) => {
+  const displayAllUsers = showFullList ? userData : userData.slice(0, 2)
+  return (
+    <Card
+      className={classNames.use(styles.section).join(className)}
+      tag="section"
+    >
+      <div className={styles.formWrapper}>
+        <div className={styles.formInnerWrapper}>
+          <Card.Title tag="h2">{title}</Card.Title>
+          {subTitle}
+          <div className={styles.userMainWrapper}>
+            {displayAllUsers?.map((item) => (
+              <div className={styles.userWrapper}>
+                <div className={styles.user}>
+                  <div className={styles.userName}>
+                    {item.person ? item.person : `Not available.`}
+                  </div>
+                  <div className={styles.userName}>
+                    {item.email ? item.email : `Not available.`}
+                  </div>
+                </div>
+                <p className={styles.subTitle}>{subDescription}</p>
+                <div className={styles.additionalInfo}>
+                  {item.description ? item.description : `Not available.`}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
+        <div className={styles.mainWarningWrapper} />
       </div>
-      <div className={styles.mainWarningWrapper} />
-    </div>
-  </Card>
-)
+      {userData.length > 2 && (
+        <Button
+          className={styles.showBtn}
+          variant="outlined"
+          onClick={toggleShowFullList}
+        >
+          {showFullList ? 'Show Less' : 'Show More'}
+        </Button>
+      )}
+    </Card>
+  )
+}
 
 export default AccessUsers
