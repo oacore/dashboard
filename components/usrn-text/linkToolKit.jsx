26e165e904dcd8114c1a2ea560becdac6d256d14
vbudko <valerij.budko@gmail.com>
2024-09-12 12:10:32 +0100

    CORE-4902: Links update

--- components/usrn-text/linkToolKit.jsx
+++ components/usrn-text/linkToolKit.jsx
@@ -7,21 +7,19 @@ import styles from './styles.module.css'
 const LinkToolKit = ({ content }) => {
   const linkToolKit =
     content.linkToolKit && content.linkToolKit.length > 3 ? (
-      <div>
-        <a
-          href={content.linkToolKit}
-          target="_blank"
-          rel="noreferrer"
-          className={styles.toolkitLink}
-        >
-          <img
-            className={classNames.use(styles.docsLink)}
-            src={toolkit}
-            alt="toolkit"
-          />
-          Go to the toolkit
-        </a>
-      </div>
+      <a
+        href={content.linkToolKit}
+        target="_blank"
+        rel="noreferrer"
+        className={styles.toolkitLink}
+      >
+        <img
+          className={classNames.use(styles.docsLink)}
+          src={toolkit}
+          alt="toolkit"
+        />
+        Go to the toolkit
+      </a>
     ) : (
       ''
     )
