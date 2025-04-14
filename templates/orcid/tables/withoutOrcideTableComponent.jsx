b2cb00d67c3eb51bfece2f4475cd43e8200063c3
ekachxaidze98 <ekachxaidze2@gmail.com>
2025-04-14 18:07:24 +0400

    CORE-5055: scroll to specific section

--- templates/orcid/tables/withoutOrcideTableComponent.jsx
+++ templates/orcid/tables/withoutOrcideTableComponent.jsx
@@ -89,121 +89,130 @@ const WithoutOrcideTableComponent = observer(
       }
     }
 
-    return initialLoad ? (
-      <div className={styles.dataSpinnerWrapper}>
-        <ProgressSpinner className={styles.spinner} />
-        <p className={styles.spinnerText}>
-          This may take a while, longer for larger repositories ...
-        </p>
+    return (
+      <div id="withoutOrcideTable">
+        {initialLoad ? (
+          <div className={styles.dataSpinnerWrapper}>
+            <ProgressSpinner className={styles.spinner} />
+            <p className={styles.spinnerText}>
+              This may take a while, longer for larger repositories ...
+            </p>
+          </div>
+        ) : (
+          <Tablev2
+            className={styles.orcidTable}
+            isHeaderClickable
+            rowIdentifier="articleId"
+            data={data}
+            size={data?.length}
+            totalLength={formatNumber(totalLength)}
+            localSearch
+            searchable
+            searchChange={onSearchChange}
+            localSearchTerm={localSearchTerm}
+            rowClick={(row) => onSetActiveArticle(row)}
+            fetchData={fetchData}
+            // excludeFooter={checkBillingType || !hasData || hasError}
+            isLoading={isLoading}
+            renderDropDown={renderDropDown}
+            details={
+              <TableArticle
+                changeVisibility={changeArticleVisibility}
+                article={articleData}
+                loading={loading}
+                outputsUrl={outputsUrl}
+                articleAdditionalDataLoading={articleAdditionalDataLoading}
+              />
+            }
+          >
+            <Table.Column
+              id="oai"
+              display="OAI"
+              getter={(v) => {
+                if (v?.oai) {
+                  return (
+                    <span className={styles.oaiCell}>
+                      {v.oai.split(':').pop()}
+                    </span>
+                  )
+                }
+                return '-'
+              }}
+              className={styles.oaiColumn}
+            />
+            <Table.Column
+              id="title"
+              display="Title"
+              getter={(v) => v?.title || '-'}
+              className={styles.titleColumn}
+            />
+            <Table.Column
+              id="authors"
+              display="Authors"
+              className={styles.authorsColumn}
+              getter={(v) =>
+                v.authors && v.authors.map((a) => a.name).join(' ')
+              }
+            />
+            <Table.Column
+              id="orcid"
+              display="ORCID ID"
+              getter={(v) => {
+                if (v?.author_pid) {
+                  return (
+                    <span className={styles.orcidCell}>
+                      <img src={idIcon} alt="idIcon" />
+                      {v?.author_pid || '-'}
+                    </span>
+                  )
+                }
+                return '-'
+              }}
+              className={styles.orcidColumn}
+            />
+            <Table.Column
+              id="publicationDate"
+              display="Publication date"
+              className={styles.publicationDateColumn}
+              getter={(v) => v?.publicationDate}
+            />
+            <Table.Column
+              id="actions"
+              getter={(v) => (
+                <div className={styles.actionButtonWrapper}>
+                  <Button
+                    className={styles.actionButtonPure}
+                    onClick={(e) => handleClick(e, v)}
+                  >
+                    <img src={kababMenu} alt="kababMenu" />
+                  </Button>
+                  <Menu
+                    ref={menuRef}
+                    visible={visibleMenu && selectedRowData === v}
+                    className={styles.menuButton}
+                    stopPropagation
+                  >
+                    {Object.values(texts.actions).map(({ title, key }) => (
+                      <Menu.Item key={key} target="_blank">
+                        {/* eslint-disable-next-line max-len */}
+                        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+                        <div
+                          onClick={(e) =>
+                            handleToggleRedirect(e, key, v.id, v.oai)
+                          }
+                          className={styles.togglerTitle}
+                        >
+                          {title}
+                        </div>
+                      </Menu.Item>
+                    ))}
+                  </Menu>
+                </div>
+              )}
+            />
+          </Tablev2>
+        )}
       </div>
-    ) : (
-      <Tablev2
-        id="withoutOrcideTable"
-        className={styles.orcidTable}
-        isHeaderClickable
-        rowIdentifier="articleId"
-        data={data}
-        size={data?.length}
-        totalLength={formatNumber(totalLength)}
-        localSearch
-        searchable
-        searchChange={onSearchChange}
-        localSearchTerm={localSearchTerm}
-        rowClick={(row) => onSetActiveArticle(row)}
-        fetchData={fetchData}
-        // excludeFooter={checkBillingType || !hasData || hasError}
-        isLoading={isLoading}
-        renderDropDown={renderDropDown}
-        details={
-          <TableArticle
-            changeVisibility={changeArticleVisibility}
-            article={articleData}
-            loading={loading}
-            outputsUrl={outputsUrl}
-            articleAdditionalDataLoading={articleAdditionalDataLoading}
-          />
-        }
-      >
-        <Table.Column
-          id="oai"
-          display="OAI"
-          getter={(v) => {
-            if (v?.oai) {
-              return (
-                <span className={styles.oaiCell}>{v.oai.split(':').pop()}</span>
-              )
-            }
-            return '-'
-          }}
-          className={styles.oaiColumn}
-        />
-        <Table.Column
-          id="title"
-          display="Title"
-          getter={(v) => v?.title || '-'}
-          className={styles.titleColumn}
-        />
-        <Table.Column
-          id="authors"
-          display="Authors"
-          className={styles.authorsColumn}
-          getter={(v) => v.authors && v.authors.map((a) => a.name).join(' ')}
-        />
-        <Table.Column
-          id="orcid"
-          display="ORCID ID"
-          getter={(v) => {
-            if (v?.author_pid) {
-              return (
-                <span className={styles.orcidCell}>
-                  <img src={idIcon} alt="idIcon" />
-                  {v?.author_pid || '-'}
-                </span>
-              )
-            }
-            return '-'
-          }}
-          className={styles.orcidColumn}
-        />
-        <Table.Column
-          id="publicationDate"
-          display="Publication date"
-          className={styles.publicationDateColumn}
-          getter={(v) => v?.publicationDate}
-        />
-        <Table.Column
-          id="actions"
-          getter={(v) => (
-            <div className={styles.actionButtonWrapper}>
-              <Button
-                className={styles.actionButtonPure}
-                onClick={(e) => handleClick(e, v)}
-              >
-                <img src={kababMenu} alt="kababMenu" />
-              </Button>
-              <Menu
-                ref={menuRef}
-                visible={visibleMenu && selectedRowData === v}
-                className={styles.menuButton}
-                stopPropagation
-              >
-                {Object.values(texts.actions).map(({ title, key }) => (
-                  <Menu.Item key={key} target="_blank">
-                    {/* eslint-disable-next-line max-len */}
-                    {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
-                    <div
-                      onClick={(e) => handleToggleRedirect(e, key, v.id, v.oai)}
-                      className={styles.togglerTitle}
-                    >
-                      {title}
-                    </div>
-                  </Menu.Item>
-                ))}
-              </Menu>
-            </div>
-          )}
-        />
-      </Tablev2>
     )
   }
 )
