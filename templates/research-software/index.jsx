494494009008b136fbb750f54b31333dbd2d716f
ekachxaidze98 <ekachxaidze2@gmail.com>
2025-04-22 13:31:11 +0400

    CORE-5136: add stats cards

--- templates/research-software/index.jsx
+++ templates/research-software/index.jsx
@@ -5,7 +5,8 @@ import { observer } from 'mobx-react-lite'
 import styles from './styles.module.css'
 import DashboardHeader from '../../components/dashboard-header'
 import ShowMoreText from '../../components/showMore'
-import text from '../../texts/sw/sw.yml'
+import texts from '../../texts/sw/sw.yml'
+import StatsCard from '../../components/statsCard/statsCard'
 
 const SwPageTemplate = observer(
   ({ tag: Tag = 'main', className, ...restProps }) => {
@@ -20,11 +21,11 @@ const SwPageTemplate = observer(
         {...restProps}
       >
         <DashboardHeader
-          title={text.title}
+          title={texts.title}
           showMore={
             <ShowMoreText
               className={styles.description}
-              text={text.description || 'N/A'}
+              text={texts.description || 'N/A'}
               maxLetters={320}
               showMore={showMore}
               toggleShowMore={toggleShowMore}
@@ -32,7 +33,44 @@ const SwPageTemplate = observer(
             />
           }
         />
-        hi
+        <div className={styles.mainWrapper}>
+          <div className={styles.cardsWrapper}>
+            <StatsCard
+              title={texts.statsCards.ready.title}
+              description={texts.statsCards.ready.description}
+              actionText={texts.statsCards.ready.action}
+              showInfo={texts.statsCards.ready.tooltip}
+              // loading={globalStore.dataProvider.orcidStatsLoading}
+              count={54}
+              wholeWidthCard
+              // actionHref="#orcideTable"
+              // onActionClick={() => handleStatsCardClick('with')}
+            />
+            <StatsCard
+              title={texts.statsCards.sent.title}
+              description={texts.statsCards.sent.description}
+              actionText={texts.statsCards.sent.action}
+              showInfo={texts.statsCards.sent.tooltip}
+              noticeable={texts.statsCards.sent.noticeable}
+              // loading={globalStore.dataProvider.orcidStatsLoading}
+              count={31}
+              wholeWidthCard
+              // actionHref="#withoutOrcideTable"
+              // onActionClick={() => handleStatsCardClick('without')}
+              countClassName={styles.inputCount}
+            />
+            <StatsCard
+              title={texts.statsCards.responded.title}
+              actionText={texts.statsCards.responded.action}
+              showInfo={texts.statsCards.responded.tooltip}
+              // loading={globalStore.dataProvider.orcidStatsLoading}
+              wholeWidthCard
+              count={12}
+              // actionHref="#otherDataTable"
+              // onActionClick={() => handleStatsCardClick('other')}
+            />
+          </div>
+        </div>
       </Tag>
     )
   }
