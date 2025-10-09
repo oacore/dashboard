03c65e28e7dd45db903d857fd470cf9d06f1cb01
ekachxaidze98 <ekachxaidze2@gmail.com>
2025-10-09 11:53:52 +0400

    CORE-5233: add reusable components to docs

--- templates/membership/docs.jsx
+++ templates/membership/docs.jsx
@@ -1,15 +1,75 @@
-import React, { useEffect, useState } from 'react'
-import { classNames } from '@oacore/design/lib/utils'
+import React, { useCallback, useEffect, useState } from 'react'
+import {
+  DocumentationMembership,
+  DocumentationMembershipNav,
+  Video,
+  DocumentSelect,
+} from '@oacore/design/lib/modules'
+import { useRouter } from 'next/router'
 
-import styles from './docs-styles.module.css'
-import DocumentationNav from '../membershipDocumentationNav'
-import Markdown from '../../components/markdown'
-import redirectLink from '../../components/upload/assets/redirectLink.svg'
-import activeArrow from '../../components/upload/assets/activeArrow.svg'
+import styles from './styles.module.css'
+import text from '../../texts/memership/membership.yml'
 
-const DocumentationBlockTemplate = ({ headerDashboard, docs, navigation }) => {
+import videoIcon from 'components/upload/assets/tutorialActive.svg'
+import redirectIcon from 'components/upload/assets/redirectLink.svg'
+// import DocumentSelect from '../../../components/docs-select'
+
+function normalizeHref(str) {
+  const test = str.replace('#', '')
+  return test.replace('_', '-')
+}
+
+const DocumentationBlockTemplate = ({ docs, navigation }) => {
   const [highlight, setHighlight] = useState()
+  const [navActiveIndex, setNavActiveIndex] = useState(null)
+  const [selectedOption, setSelectedOption] = useState(
+    text.documentationSwitcher[0].title
+  )
   const [showNavigator, setShowNavigator] = useState(false)
+  const [visibleVideo, setVisibleVideo] = useState(null)
+
+  const handleContentOpen = useCallback((condition) => {
+    if (condition) setVisibleVideo(condition)
+  }, [])
+
+  const route = useRouter()
+  const headerHeight = 56
+
+  useEffect(() => {
+    const { hash } = window.location
+    const id = hash.substring(1)
+    const element = document.getElementById(id)
+    setTimeout(() => {
+      if (element) {
+        const rect = element.getBoundingClientRect()
+        window.scrollTo({
+          top: rect.top + window.scrollY - headerHeight,
+          behavior: 'smooth',
+          block: 'center',
+        })
+        const n = docs?.items?.findIndex((item) => item.id === id)
+        setHighlight(n)
+      }
+    }, 100)
+  }, [route.asPath])
+
+  useEffect(() => {
+    const id = route.query?.r
+    if (id) {
+      const n = navigation.navItems.findIndex(
+        (item) => normalizeHref(item.href) === id
+      )
+      setNavActiveIndex(n)
+    }
+  }, [])
+
+  const handleButtonClick = () => {
+    route.push('membership-documentation')
+  }
+  const handleSelectChange = (option) => {
+    setSelectedOption(option)
+    if (option === 'Membership Documentation') handleButtonClick()
+  }
 
   const handleScrollToTop = () => {
     window.scrollTo({
@@ -31,101 +91,50 @@ const DocumentationBlockTemplate = ({ headerDashboard, docs, navigation }) => {
   }, [])
 
   return (
-    <div className={styles.documentationWrapper}>
-      <h2 className={styles.documentationHeader}>
-        {headerDashboard.header.title}
-      </h2>
-      <Markdown>{headerDashboard.header.caption}</Markdown>
-      <div className={styles.placement}>
-        <DocumentationNav navigation={navigation} setHighlight={setHighlight} />
-        <div className={styles.documentationInnerWrapper}>
-          {docs.items.map((item, index) => (
-            <div
-              key={item.id}
-              className={styles.documentationItem}
-              id={item.id}
-            >
-              <h3
-                // className={styles.documentationItemTitle}
-                className={classNames.use(styles.documentationItemTitle, {
-                  [styles.highlighted]: highlight === index,
-                })}
-              >
-                {item.title}
-              </h3>
-              <div className={styles.subTitleWrapper}>
-                <div className={styles.typeWrapper}>
-                  {item?.membership?.map((member) => (
-                    <span
-                      key={member.name}
-                      className={classNames
-                        .use(styles.membership)
-                        .join(member.status ? styles.enabled : styles.disabled)}
-                    >
-                      {member.name}
-                    </span>
-                  ))}
-                </div>
-                {item.redirect && (
-                  <a
-                    target="_blank"
-                    href={item.redirect?.link}
-                    className={styles.linkWrapper}
-                    rel="noreferrer"
-                  >
-                    <span className={styles.linkText}>
-                      {item.redirect?.text}
-                    </span>
-                    <img alt="redirect" src={redirectLink} />
-                  </a>
-                )}
-              </div>
-              <Markdown>{item.descriptionDashboard}</Markdown>
-              <div>
-                {item?.images?.map((img) => (
-                  <div className={styles.cardWrapper}>
-                    <div
-                      className={classNames.use({
-                        [styles.imgWrapper]: img.source,
-                      })}
-                    >
-                      {/* eslint-disable-next-line max-len */}
-                      {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-                      <img
-                        key={img.file}
-                        className={classNames.use(styles.image, {
-                          [styles.logoBanner]: item.id === 'logo-banner',
-                          [styles.logoPersonalised]:
-                            item.id === 'personalised-banner',
-                          [styles.badgeImage]: img.source,
-                          [styles.badgeImageHeight]:
-                            img.source?.includes('square'),
-                        })}
-                        src={img.file}
-                        alt="image"
-                      />
-                    </div>
-                    {img.source && (
-                      <div className={styles.textAlignment}>
-                        <span className={styles.text}>{img.source}</span>
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+    <div>
+      <div className={styles.navWrapper}>
+        <div className={styles.navTitle}>
+          <span>CORE DOCUMENTATION:</span>
         </div>
-        {showNavigator && (
-          // eslint-disable-next-line max-len
-          // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
-          <div className={styles.navigator} onClick={handleScrollToTop}>
-            <img
-              src={activeArrow}
-              alt="Logo"
-              className={styles.navigatorLogo}
+        <div className={styles.selectWrapper}>
+          <DocumentSelect
+            list={[
+              text.documentationSwitcher[0].title,
+              text.documentationSwitcher[1].title,
+            ]}
+            handleSelect={handleSelectChange}
+            selectedOption={selectedOption}
+          />
+        </div>
+      </div>
+      <div className={styles.docsLayout}>
+        <DocumentationMembership
+          docs={docs?.items}
+          handleContentOpen={handleContentOpen}
+          highlight={highlight}
+          setHighlight={setHighlight}
+          docsTitle={text.documentationSwitcher[0].title}
+          mulltyDocs
+          videoIcon={videoIcon}
+          redirectLink={redirectIcon}
+          showNavigator={showNavigator}
+          handleScrollToTop={handleScrollToTop}
+          nav={
+            <DocumentationMembershipNav
+              activeIndex={navActiveIndex}
+              setNavActiveIndex={setNavActiveIndex}
+              textData={navigation}
+              setHighlight={setHighlight}
+              mulltyDocs
             />
-          </div>
+          }
+        />
+        {visibleVideo && (
+          <Video
+            visibleModal={visibleVideo}
+            closeModal={() => setVisibleVideo(false)}
+            video={visibleVideo}
+          />
         )}
       </div>
     </div>
