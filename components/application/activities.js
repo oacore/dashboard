063ed36b2fbfc2e055846ab369c16fed12aa1d25
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2025-10-09 18:08:24 +0400

    Core 5233 data providers guide (#723)
    * CORE-5233: add reusable components to docs
    
    * CORE-5233: add data providers guide
    
    * CORE-5233: fix lint
    
    * CORE-5233: fix lint
    
    * CORE: fix redirect issue
    
    * CORE: add indicator

--- components/application/activities.js
+++ components/application/activities.js
@@ -118,7 +118,7 @@ const config = new RouteConfig([
         test: /\/recommender/,
         path: 'documentation',
         id: 'documentation',
-        value: 'Documentation',
+        value: 'Documentations',
       },
       {
         test: /\/badges/,
