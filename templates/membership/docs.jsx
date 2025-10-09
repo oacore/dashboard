32b72667d3198a9fe17029f38a996a0098f739fc
ekachxaidze98 <ekachxaidze2@gmail.com>
2025-10-09 15:36:09 +0400

    CORE-5233: fix lint

--- templates/membership/docs.jsx
+++ templates/membership/docs.jsx
@@ -63,22 +63,15 @@ const DocumentationBlockTemplate = ({
   useEffect(() => {
     const id = route.query?.r
     if (id) {
-      const currentNavigation =
-        selectedOption === text.documentationSwitcher[1].title
-          ? navigation
-          : dataProviderDocs.navigation
-
-      const n = currentNavigation.navItems.findIndex(
+      const n = navigation.navItems.findIndex(
         (item) => normalizeHref(item.href) === id
       )
       setNavActiveIndex(n)
     }
-  }, [selectedOption, navigation, dataProviderDocs?.navigation, route.query?.r])
+  }, [])
 
   const handleSelectChange = (option) => {
     setSelectedOption(option)
-    // Reset navigation active index when switching between documentation types
-    setNavActiveIndex(null)
   }
 
   const handleScrollToTop = () => {
