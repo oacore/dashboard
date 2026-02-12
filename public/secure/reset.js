65c735df0607bb683c949fbdca73c7d4d3360ed7
Vako <55918472+MrDarkG@users.noreply.github.com>
2026-02-12 20:11:37 +0400

    hf/reset-password-page-request (#748)
    * hf/reset-password-page-request
    
    * hf/reset-password-page-request
    added console log

--- public/secure/reset.js
+++ public/secure/reset.js
@@ -70,8 +70,8 @@ function changePassword(event) {
   const formData = new FormData(event.target)
   const data = {
     email: formData.get('email'),
-    password: formData.get('password'),
-    newPassword: formData.get('newPassword'),
+    password: formData.get('newPassword'),
+    newPassword: formData.get('newPasswordAgain'),
     confirmationToken: formData.get('token'),
   }
 
@@ -100,6 +100,7 @@ function changePassword(event) {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
+  console.log("hello world")
   form.addEventListener('submit', changePassword)
   newPassword.oninput = checkNewPasswordsMatch
   newPasswordAgain.oninput = checkNewPasswordsMatch
