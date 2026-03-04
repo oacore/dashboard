eeba0130234174b1326e8792ef1034d90cfce541
ekachxaidze98 <ekachxaidze2@gmail.com>
2026-03-04 21:45:49 +0400

    CORE: fix build issue

--- src/guards/ProtectedRoute.tsx
+++ src/guards/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import  { useEffect } from 'react';
 import type { ReactNode } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthStore } from '@/store/authStore.ts';
